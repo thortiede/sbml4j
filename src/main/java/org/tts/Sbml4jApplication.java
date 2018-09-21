69cfa4c207acbef53c2a8016ad05be9e1ba3af34
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2018-09-21 16:20:00 +0200

    Spring Starter Project with JAX-RS (Jersey) and Neo4j support

---
+++ src/main/java/org/tts/Sbml4jApplication.java
@@ -0,0 +1,12 @@
+package org.tts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbml4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbml4jApplication.class, args);
+	}
+}
