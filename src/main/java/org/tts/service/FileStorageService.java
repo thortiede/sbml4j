c9b8e4c9777408171669e60249303e06d1efb938
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2018-09-28 21:46:34 +0200

    Changed Resource import to core.io

--- src/main/java/org/tts/service/FileStorageService.java
+++ src/main/java/org/tts/service/FileStorageService.java
@@ -9,7 +9,7 @@ import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.UrlResource;
-import org.springframework.hateoas.Resource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
