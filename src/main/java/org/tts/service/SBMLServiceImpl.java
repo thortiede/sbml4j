9c1defb16996727ba01a464a0bba45110e2d7e5a
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-02-09 00:29:40 +0100

    use MultipartFile.Inputstream to read contents. SBMLDocument needs work

--- src/main/java/org/tts/service/SBMLServiceImpl.java
+++ src/main/java/org/tts/service/SBMLServiceImpl.java
@@ -16,10 +16,13 @@ import org.sbml.jsbml.SBMLReader;
 import org.sbml.jsbml.SBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.tts.model.SBMLDocumentEntity;
 import org.tts.model.SBMLModelEntity;
 import org.tts.model.SBMLSBaseEntity;
 
+@Service
 public class SBMLServiceImpl implements SBMLService {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -37,8 +40,9 @@ public class SBMLServiceImpl implements SBMLService {
 	}
 
 	@Override
-	public Model extractSBMLModel(File file) throws XMLStreamException, IOException {
-		SBMLDocument doc = SBMLReader.read(file);
+	public Model extractSBMLModel(MultipartFile file) throws XMLStreamException, IOException {
+		SBMLDocument doc = SBMLReader.read(file.getInputStream());
+		
 		return doc.getModel();
 		
 	}
@@ -46,8 +50,25 @@ public class SBMLServiceImpl implements SBMLService {
 	@Override
 	public Map<String, Iterable<SBMLSBaseEntity>> extractSBMLEntities(Model model) {
 		
+		/*
+		 * while building all the entities
+		 * keep a table to connect the uuid of an entity
+		 * with the id from the sbml document by which this entity
+		 * is referenced in the model.
+		 * That way, once I need to reference an entity and only have the 
+		 * sbml-internal id, I can look up the respective uuid,
+		 * find that element by uuid in the respective Iterable
+		 * and link to it.
+		 * Or something like this.
+		 */
+		
 		Map<String, Iterable<SBMLSBaseEntity>> allModelEntities = new HashMap<>();
 		
+		// sbmlDocument does not make sense. 
+		// either we use it to save all declared namespaces
+		// which correspond to the extensions used
+		// or we skip it.
+		// check again.
 		SBMLDocumentEntity sbmlDocument = new SBMLDocumentEntity();
 		
 		setSbaseProperties(model.getSBMLDocument(), sbmlDocument);
@@ -60,12 +81,7 @@ public class SBMLServiceImpl implements SBMLService {
 		
 		SBMLModelEntity sbmlModelEntity = new SBMLModelEntity();
 		setSbaseProperties(model, sbmlModelEntity);
-		sbmlModelEntity.setAreaUnits(model.getAreaUnits());
-		sbmlModelEntity.setConversionFactor(model.getConversionFactor());
-		sbmlModelEntity.setEnclosingSBMLEntity(sbmlDocument);
-		// set more model specific values
-		
-		// then make List
+		setModelProperties(model, sbmlDocument, sbmlModelEntity);
 		List<SBMLSBaseEntity> sbmlModelList = new ArrayList<>();
 		sbmlModelList.add(sbmlModelEntity);
 		allModelEntities.put("SBMLModelEntity", sbmlModelList);
@@ -73,6 +89,26 @@ public class SBMLServiceImpl implements SBMLService {
 		
 		
 		return null;
+	}
+
+	private void setModelProperties(Model model, SBMLDocumentEntity sbmlDocument, SBMLModelEntity sbmlModelEntity) {
+		/**
+		 * The following string attributes contain IDRefs to 
+		 * UnitDefinitions.
+		 * TODO: actually link to the UnitDefinition-Object here.
+		 * Since current queries don't use them, we skip them for now
+		 * and only save the ids to the graph
+		 * Once we load the UnitDefinitions
+		 * a) manual linking is possible
+		 * b) linking here on creation or on a second pass becomes possible.
+		 */
+		sbmlModelEntity.setSubstanceUnits(model.getSubstanceUnits());
+		sbmlModelEntity.setTimeUnits(model.getTimeUnits());
+		sbmlModelEntity.setVolumeUnits(model.getVolumeUnits());
+		sbmlModelEntity.setAreaUnits(model.getAreaUnits());
+		sbmlModelEntity.setLengthUnits(model.getLengthUnits());
+		sbmlModelEntity.setConversionFactor(model.getConversionFactor());
+		sbmlModelEntity.setEnclosingSBMLEntity(sbmlDocument);
 	}
 
 	private void setSbaseProperties(SBase source, SBMLSBaseEntity target) {
