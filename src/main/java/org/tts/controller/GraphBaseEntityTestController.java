cfb7a3bae20a330b942f72e67921afd5ff6c84f9
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-02-08 19:50:18 +0100

    test creating SBMLSbase through graphBaseEntityService

--- src/main/java/org/tts/controller/GraphBaseEntityTestController.java
+++ src/main/java/org/tts/controller/GraphBaseEntityTestController.java
@@ -1,10 +1,13 @@
 package org.tts.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.tts.model.GraphBaseEntity;
 import org.tts.model.SBMLSBaseEntity;
@@ -20,21 +23,51 @@ public class GraphBaseEntityTestController {
 		super();
 		this.graphBaseEntityService = graphBaseEntityService;
 	}
+	/**
+	 * Test GraphBaseEntity
+	 */
+
+	@RequestMapping(value = "/testListBaseEntities", method=RequestMethod.GET)	
+	public Iterable<GraphBaseEntity> getBaseEntities() {
+		
+		return graphBaseEntityService.findAll();
+	}
 	
-	@RequestMapping(value = "/testCreateEntity", method=RequestMethod.GET)	
+	
+	@RequestMapping(value = "/testBaseEntity", method=RequestMethod.GET)	
+	public GraphBaseEntity getNewBaseEntity(@PathVariable String uuid) {
+		
+		return graphBaseEntityService.findByEntityUUID(uuid);
+	}
+	
+	@RequestMapping(value = "/testBaseEntity", method=RequestMethod.POST)	
 	public GraphBaseEntity createNewBaseEntity() {
 		GraphBaseEntity newEntity = new GraphBaseEntity();
 		newEntity.setEntityUUID(UUID.randomUUID().toString());
 		return graphBaseEntityService.persistEntity(newEntity);
 	}
 	
-	@RequestMapping(value = "/testCreateSBase", method=RequestMethod.GET)	
-	public GraphBaseEntity createNewSBaseEntity() {
+	/**
+	 * Test SBMLSBaseEntity
+	 */
+	
+	
+	@RequestMapping(value = "/testSBase", method=RequestMethod.POST)	
+	public GraphBaseEntity createNewSBaseEntity(@RequestParam(value = "sbaseId") String sBaseId,
+												@RequestParam(value = "sbaseName") String sBaseName) {
 		SBMLSBaseEntity newEntity = new SBMLSBaseEntity();
 		newEntity.setEntityUUID(UUID.randomUUID().toString());
-		newEntity.setSbaseId("123");
+		newEntity.setSbaseId(sBaseId);
+		newEntity.setsBaseName(sBaseName);
 		return graphBaseEntityService.persistEntity(newEntity);
 	}
+	
+	@RequestMapping(value = "/testSBase/{uuid}", method=RequestMethod.GET)
+	public GraphBaseEntity getGraphBaseSBase(@PathVariable String uuid) {
+		return graphBaseEntityService.findByEntityUUID(uuid);
+	}
+	
+	
 	
 	
 	
