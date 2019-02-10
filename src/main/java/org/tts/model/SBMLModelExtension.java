9b44d323cfc1d5a460e7f5b204b1e7e7f85a5683
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-02-10 23:09:20 +0100

    Change to Inheritance instead of implementing interface

--- src/main/java/org/tts/model/SBMLModelExtension.java
+++ src/main/java/org/tts/model/SBMLModelExtension.java
@@ -1,89 +1,19 @@
 package org.tts.model;
 
-import java.util.List;
 
 import org.neo4j.ogm.annotation.Relationship;
 
 
-public class SBMLSBaseExtensionQual extends SBMLSBaseEntity implements SBMLSBaseExtension, SBMLModelExtension {
-
-	@Relationship(type = "CONTAINS", direction = Relationship.OUTGOING)
-	List<SBMLQualSpecies> sbmlQualSpecies;
-	
-	@Relationship(type = "CONTAINS", direction = Relationship.OUTGOING)
-	List<SBMLQualTransition> sbmlQualTransitions;
+public class SBMLModelExtension extends SBMLSBaseExtension {
 
 	@Relationship(type = "IS_EXTENSION_OF", direction = Relationship.OUTGOING)
 	public SBMLModelEntity parentModel;
-	
-	private String namespaceURI;
-	
-	private String shortLabel = "qual";
-	
-	private boolean required = false;
-	 
-	public List<SBMLQualSpecies> getSbmlQualSpecies() {
-		return sbmlQualSpecies;
-	}
 
-	public void setSbmlQualSpecies(List<SBMLQualSpecies> sbmlQualSpecies) {
-		this.sbmlQualSpecies = sbmlQualSpecies;
-	}
-
-	public List<SBMLQualTransition> getSbmlQualTransitions() {
-		return sbmlQualTransitions;
-	}
-
-	public void setSbmlQualTransitions(List<SBMLQualTransition> sbmlQualTransitions) {
-		this.sbmlQualTransitions = sbmlQualTransitions;
-	}
-
-	@Override
-	public boolean extendsModel() {
-		return true;
-	}
-
-	@Override
-	public boolean extendsSBML() {
-		return false;
-	}
-
-	@Override
-	public boolean isRequired() {
-		return this.required;
-	}
-	
-	public void setRequired(boolean required) {
-		this.required = required;
-	}
-
-	@Override
-	public String getShortLabel() {
-		return this.shortLabel;
-	}
-	
-	@Override
-	public void setShortLabel(String shortLabel) {
-		this.shortLabel = shortLabel;		
-	}
-
-	@Override
-	public String getNamespaceURI() {
-		return this.namespaceURI;
-	}
-	
-	@Override
-	public void setNamespaceURI(String namespaceURI) {
-		this.namespaceURI = namespaceURI;
-	}
-
-	@Override
 	public SBMLModelEntity getParentModel() {
-		return this.parentModel;
+		return parentModel;
 	}
 	
-	public void setParentModel(SBMLModelEntity parent) {
-		this.parentModel = parent;
+	public void setParentModel(SBMLModelEntity parentModel) {
+		this.parentModel = parentModel;
 	}
-
 }
