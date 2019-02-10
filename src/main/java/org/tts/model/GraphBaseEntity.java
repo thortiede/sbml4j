b400a7460f773d3c286fb735b731104ea3bd3ebe
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-02-10 23:03:17 +0100

    Remove NodeEntity to alaso use it for RelationshipEntities

--- src/main/java/org/tts/model/GraphBaseEntity.java
+++ src/main/java/org/tts/model/GraphBaseEntity.java
@@ -1,13 +1,11 @@
 package org.tts.model;
 
-import java.util.UUID;
-
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Version;
 
-@NodeEntity
+
 public class GraphBaseEntity {
 
 	@Id @GeneratedValue
@@ -21,6 +19,22 @@ public class GraphBaseEntity {
 	 * Entities shall reference other entities only by this UUID
 	 */
 	private String entityUUID;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
 
 	public String getEntityUUID() {
 		return entityUUID;
