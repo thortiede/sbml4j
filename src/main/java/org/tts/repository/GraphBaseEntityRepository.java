426fdefce6ee6e4f0ac9d63543235555d807b8bd
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-02-13 14:23:49 +0100

    existsByEntityUUID added

--- src/main/java/org/tts/repository/GraphBaseEntityRepository.java
+++ src/main/java/org/tts/repository/GraphBaseEntityRepository.java
@@ -1,11 +1,14 @@
 package org.tts.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.tts.model.GraphBaseEntity;
+import org.tts.model.SBMLCompartment;
 
 public interface GraphBaseEntityRepository extends Neo4jRepository<GraphBaseEntity, Long> {
 
 	GraphBaseEntity findByEntityUUID(String entityUUID);
+
+	boolean existsByEntityUUID(String entityUUID);
+
+	
 }
