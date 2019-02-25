f22236e94847fbf8a0e3defbd61bd9d2b5865397
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-02-25 22:39:43 +0100

    collect all ppi transitions

--- src/main/java/org/tts/service/NetworkMappingServiceImpl.java
+++ src/main/java/org/tts/service/NetworkMappingServiceImpl.java
@@ -2,6 +2,8 @@ package org.tts.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tts.model.NodeEdgeList;
@@ -19,6 +21,9 @@ public class NetworkMappingServiceImpl implements NetworkMappingService {
 		
 
 	}*/
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	public NetworkMappingServiceImpl(GraphBaseEntityRepository graphBaseEntityRepository) {
 		super();
@@ -27,19 +32,62 @@ public class NetworkMappingServiceImpl implements NetworkMappingService {
 
 	@Override
 	public NodeEdgeList getProteinInteractionNetwork(List<String> interactionTypes) {
+		String[] bqList = {"BQB_IS", "BQB_HAS_VERSION"};
+		String[] functionList = {"noGroup", "groupLeft", "groupRight", "groupBoth"};
+		NodeEdgeList allInteractions = new NodeEdgeList();
 		if(interactionTypes == null) {
 			// no filter given, return network with all interaction types
-			Iterable<NodeNodeEdge> isIsInteractions = graphBaseEntityRepository.getInteractionCustom1("BQB_IS", "BQB_IS");
-			NodeEdgeList allInteractions = new NodeEdgeList();
-			for (NodeNodeEdge nne : isIsInteractions) {
-				allInteractions.addListEntry(nne.getNode1(), nne.getNode2(), nne.getEdge());
+			Iterable<NodeNodeEdge> loopNNE;
+			for(String bqType1 : bqList) {
+				for (String  bqType2 : bqList) {
+					for (String functionName : functionList) {
+						int i = 0;
+						loopNNE = getTransitionsBetween(bqType1, bqType2, functionName);
+						for (NodeNodeEdge nne : loopNNE) {
+							allInteractions.addListEntry(nne.getNode1(), nne.getNode2(), nne.getEdge());
+							i++;
+						}
+						logger.info(bqType1 + "_" + bqType2 + "_" + functionName + "_interactions: " + i);
+					}
+				}
 			}
+			
+			
+			
+			
 			return allInteractions;
 		} else {
 			// filter the network by the given types.
 			// how would you implement that efficiently?
 		}
 		return null;
+	}
+
+	/**
+	 * @param bqType1
+	 * @param bqType2
+	 * @param functionName
+	 * @return
+	 */
+	private Iterable<NodeNodeEdge> getTransitionsBetween(String bqType1, String bqType2, String functionName) {
+		Iterable<NodeNodeEdge> interactions;
+		switch (functionName) {
+		case "noGroup":
+			interactions = graphBaseEntityRepository.getInteractionCustomNoGroup(bqType1, bqType2);
+			break;
+		case "groupLeft":
+			interactions = graphBaseEntityRepository.getInteractionCustomGroupOnInput(bqType1, bqType2);
+			break;
+		case "groupRight":
+			interactions = graphBaseEntityRepository.getInteractionCustomGroupOnOutput(bqType1, bqType2);
+			break;
+		case "groupBoth":
+			interactions = graphBaseEntityRepository.getInteractionCustomGroupOnBoth(bqType1, bqType2);
+			break;
+		default:
+			return null;
+		}
+		return interactions;
 	}
 
 	@Override
