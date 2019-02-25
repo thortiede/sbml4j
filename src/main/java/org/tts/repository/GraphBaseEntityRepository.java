4243cb1682ae663e79a6eb57c45ffb2e16885a89
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-02-25 19:27:26 +0100

    Custom nodenodeedge query for simpletransition

--- src/main/java/org/tts/repository/GraphBaseEntityRepository.java
+++ src/main/java/org/tts/repository/GraphBaseEntityRepository.java
@@ -1,7 +1,9 @@
 package org.tts.repository;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.tts.model.GraphBaseEntity;
+import org.tts.model.NodeNodeEdge;
 import org.tts.model.SBMLCompartment;
 
 public interface GraphBaseEntityRepository extends Neo4jRepository<GraphBaseEntity, Long> {
@@ -10,5 +12,7 @@ public interface GraphBaseEntityRepository extends Neo4jRepository<GraphBaseEnti
 
 	boolean existsByEntityUUID(String entityUUID);
 
-	
+	@Query(value = "match (e2:ExternalResourceEntity)-[:BQ {qualifier: {0}}]-(s2:SBMLSpecies)-[:IS]-(q2:SBMLQualSpecies)-[:IS_INPUT]-(t:SBMLSimpleTransition)-[:IS_OUTPUT]-(q:SBMLQualSpecies)-[:IS]-(s:SBMLSpecies)-[:BQ {qualifier: {1}}]-(e:ExternalResourceEntity) where e.type = \"kegg.genes\" and e2.type = \"kegg.genes\" return e2.name as node1, t.sBaseSboTerm as edge, e.name as node2")
+	Iterable<NodeNodeEdge> getInteractionCustom1(String inputExternalRel, String outputExternalRel);
+
 }
