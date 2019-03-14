bff24a6ae9a51810bb277bc4afe620e87a3e4225
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-03-14 13:50:15 +0100

    add commandLineRunner

--- src/main/java/org/tts/Sbml4jApplication.java
+++ src/main/java/org/tts/Sbml4jApplication.java
@@ -1,8 +1,13 @@
 package org.tts;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.tts.property.FileStorageProperties;
 
 @SpringBootApplication
@@ -13,5 +18,20 @@ public class Sbml4jApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sbml4jApplication.class, args);
+	}
+	
+	@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
 	}
 }
