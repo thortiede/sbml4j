e9d20c2fed8a2408d79bd9aa669810ebce7fe71a
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-03-22 22:45:40 +0100

    simpleReaction Entity for the simpleModel

---
+++ src/main/java/org/tts/model/SBMLSimpleReaction.java
@@ -0,0 +1,74 @@
+package org.tts.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.neo4j.ogm.annotation.Relationship;
+
+public class SBMLSimpleReaction extends SBMLCompartmentalizedSBaseEntity {
+	
+	private boolean reversible;
+	
+	@Relationship(type = "IS_REACTANT", direction = Relationship.OUTGOING)
+	private List<SBMLSpecies> reactants;
+	
+	@Relationship(type = "IS_PRODUCT", direction = Relationship.OUTGOING)
+	private List<SBMLSpecies> products;
+	
+	@Relationship(type = "IS_CATALYST", direction = Relationship.OUTGOING)
+	private List<SBMLSpecies> catalysts;
+
+	public boolean isReversible() {
+		return reversible;
+	}
+
+	public void setReversible(boolean reversible) {
+		this.reversible = reversible;
+	}
+
+	public List<SBMLSpecies> getReactants() {
+		return reactants;
+	}
+
+	public void setReactants(List<SBMLSpecies> reactants) {
+		this.reactants = reactants;
+	}
+
+	public void addReactant(SBMLSpecies species) {
+		if (this.reactants == null) {
+			this.reactants = new ArrayList<>();
+		}
+		this.reactants.add(species);
+	}
+	
+	public List<SBMLSpecies> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<SBMLSpecies> products) {
+		this.products = products;
+	}
+
+	public void addProduct(SBMLSpecies species) {
+		if (this.products == null) {
+			this.products = new ArrayList<>();
+		}
+		this.products.add(species);
+	}
+	
+	public List<SBMLSpecies> getCatalysts() {
+		return catalysts;
+	}
+
+	public void setCatalysts(List<SBMLSpecies> catalysts) {
+		this.catalysts = catalysts;
+	}
+	
+	public void addCatalysts(SBMLSpecies species) {
+		if (this.catalysts == null) {
+			this.catalysts = new ArrayList<>();
+		}
+		this.catalysts.add(species);
+	}
+	
+}
