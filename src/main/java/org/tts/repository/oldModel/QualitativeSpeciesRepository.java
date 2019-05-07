400b1a7a64737c8fdf6529098a3e5c11d4d093f9
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-05-07 15:31:07 +0200

    remove old model entities with its service/repo/controller layer

--- src/main/java/org/tts/repository/oldModel/QualitativeSpeciesRepository.java
+++
@@ -1,9 +0,0 @@
-package org.tts.repository.oldModel;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.tts.model.old.GraphQualitativeSpecies;
-
-public interface QualitativeSpeciesRepository extends Neo4jRepository<GraphQualitativeSpecies, Long> {
-
-	GraphQualitativeSpecies getBySbmlIdString(String sbmlIdString);
-
-}
--- src/main/java/org/tts/repository/oldModel/SpeciesRepository.java
+++
@@ -1,9 +0,0 @@
-package org.tts.repository.oldModel;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.tts.model.old.GraphSpecies;
-
-public interface SpeciesRepository extends Neo4jRepository<GraphSpecies, Long> {
-
-	GraphSpecies getBySbmlIdString(String sbmlIdString);
-
-}
