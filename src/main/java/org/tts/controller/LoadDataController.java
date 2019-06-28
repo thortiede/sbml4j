68748473d1c7bb23e1b30954dacae9dd5bc1b63c
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-06-28 16:36:16 +0200

    handle Provenance and Warehouse Entities; change to ProvenanceEntity

--- src/main/java/org/tts/controller/LoadDataController.java
+++ src/main/java/org/tts/controller/LoadDataController.java
@@ -15,19 +15,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.tts.model.common.GraphBaseEntity;
-import org.tts.model.common.SBMLFile;
+import org.tts.model.common.GraphEnum.FileNodeType;
+import org.tts.model.common.GraphEnum.ProvenanceGraphActivityType;
+import org.tts.model.common.GraphEnum.ProvenanceGraphAgentType;
+import org.tts.model.common.GraphEnum.ProvenanceGraphEdgeType;
+import org.tts.model.common.GraphEnum.WarehouseGraphEdgeType;
+import org.tts.model.common.Organism;
+import org.tts.model.provenance.ProvenanceEntity;
+import org.tts.model.provenance.ProvenanceGraphActivityNode;
+import org.tts.model.provenance.ProvenanceGraphAgentNode;
+import org.tts.model.warehouse.DatabaseNode;
+import org.tts.model.warehouse.FileNode;
+import org.tts.model.warehouse.PathwayNode;
 import org.tts.service.FileCheckService;
 import org.tts.service.FileService;
 import org.tts.service.FileStorageService;
 import org.tts.service.NodeEdgeListService;
+import org.tts.service.OrganismService;
+import org.tts.service.ProvenanceGraphService;
 import org.tts.service.SBMLPersistenceService;
 import org.tts.service.SBMLService;
+import org.tts.service.WarehouseGraphService;
 
 @RestController
 public class LoadDataController {
@@ -38,7 +53,10 @@ public class LoadDataController {
 	SBMLPersistenceService sbmlPersistenceService;
 	FileService fileService;
 	NodeEdgeListService nodeEdgeListService;
-
+	OrganismService organismService;
+	ProvenanceGraphService provenanceGraphService;
+	WarehouseGraphService warehouseGraphService;
+	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
@@ -47,7 +65,10 @@ public class LoadDataController {
 			@Qualifier("SBMLSimpleModelServiceImpl") SBMLService sbmlService, 
 			SBMLPersistenceService sbmlPersistenceService,
 			FileService fileService,
-			@Qualifier("simpleNodeEdgeListServiceImpl") NodeEdgeListService nodeEdgeListService) {
+			@Qualifier("simpleNodeEdgeListServiceImpl") NodeEdgeListService nodeEdgeListService,
+			OrganismService organismService,
+			ProvenanceGraphService provenanceGraphService,
+			WarehouseGraphService warehouseGraphService) {
 		super();
 		this.fileStorageService = fileStorageService;
 		this.fileCheckService = fileCheckService;
@@ -55,6 +76,9 @@ public class LoadDataController {
 		this.sbmlPersistenceService = sbmlPersistenceService;
 		this.fileService = fileService;
 		this.nodeEdgeListService = nodeEdgeListService;
+		this.organismService = organismService;
+		this.provenanceGraphService = provenanceGraphService;
+		this.warehouseGraphService = warehouseGraphService;
 	}
 	/** 
 	 * All Entity Endpoints
@@ -93,63 +117,143 @@ public class LoadDataController {
 	 * @return all Entities as they were persisted (or connected if already present) as a result of persisting the model
 	 */
 	@RequestMapping(value = "/sbml", method=RequestMethod.POST)
-	public ResponseEntity<List<GraphBaseEntity>> uploadSBML(@RequestParam("file") MultipartFile file,
+	public ResponseEntity<List<ProvenanceEntity>> uploadSBML(@RequestParam("file") MultipartFile file,
 															@RequestParam("organism") String organism,
 															@RequestParam("matchingAttribute") String matchingAttribute,
 															@RequestParam("source") String source,
-															@RequestParam("version") String sourceVersion) {
+															@RequestParam("version") String sourceVersion,
+															@RequestHeader("user") String username) {
 		
-		SBMLFile sbmlFileNode = null;
-		List<String> newEntityLabels = new ArrayList<>();
-		Map<String, Object> newEntityAnnotations = new HashMap<>();
-		// organism
-		if(organism != null) {
-			newEntityAnnotations.put("organism", organism);
-		}
-		
-		// source and version
-		// is this source and version already created (i.e. a model from this source has been loaded?)
-		//  if yes, 
-		//		is model already loaded?
-		//			no: then connect to that source using the matching attribute for connecting to already loaded entities
-		//			yes: check identity of filecontent -> if same, model is loaded, return that model, 
-		//												  if not, throw version conflict;  
-		//  else, source not loaded
-		//     		then create new entry (source node with that version)
-		//
-		// simple model lacks the pathway node -> either create it, or use annotation/label?
+
+		/*
+		 * Basic checks of the file
+		 */
+		List<ProvenanceEntity> returnList = new ArrayList<>();
+		ProvenanceEntity defaultReturnEntity = new ProvenanceEntity();
 		
 		// can we access the files name and ContentType?
-		List<GraphBaseEntity> returnList = new ArrayList<>();
-		GraphBaseEntity defaultReturnEntity = new GraphBaseEntity();
+		
 		if(!fileCheckService.isFileReadable(file)) {
-			defaultReturnEntity.setEntityUUID("Cannot read file");
+			defaultReturnEntity.setEntityUUID("Filename or content-type not accessible");
 			returnList.add(defaultReturnEntity);
-			return new ResponseEntity<List<GraphBaseEntity>>(returnList, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<List<ProvenanceEntity>>(returnList, HttpStatus.BAD_REQUEST);
 		}
+		
 		logger.info("Serving POST /sbml for File " + file.getOriginalFilename());
+		
 		// is Content Type xml?
 		if(!fileCheckService.isContentXML(file)) {
 			defaultReturnEntity.setEntityUUID("File ContentType is not application/xml");
 			returnList.add(defaultReturnEntity);
-			return new ResponseEntity<List<GraphBaseEntity>>(returnList, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<List<ProvenanceEntity>>(returnList, HttpStatus.BAD_REQUEST);
 		}
+		
+		/*
+		 * Create Provenance Nodes
+		 */
+		
+		
+		// Potentially read out header information to get the user that uploads this file
+		// Create a ProvenanceGraph.Agent Node with it (using the timestamp to make it unique?) - or find Agent in DB and use it
+		// String username = "Thor"; // TODO: Grab this from the header
+		Map<String, Object> agentNodeProperties = new HashMap<>();
+		agentNodeProperties.put("graphagentname", username);
+		agentNodeProperties.put("graphagenttype", ProvenanceGraphAgentType.User);
+		ProvenanceGraphAgentNode userAgentNode = this.provenanceGraphService.createProvenanceGraphAgentNode(agentNodeProperties);
+		
+		// Then we need an Activity Node (uploadSBML)
+		//Instant createDate = Instant.now();
+		Map<String, Object> activityNodeProvenanceProperties = new HashMap<>();
+		activityNodeProvenanceProperties.put("graphactivitytype", ProvenanceGraphActivityType.persistFile);
+		activityNodeProvenanceProperties.put("graphactivityname", file.getOriginalFilename()); 
+		//activityNodeProvenanceProperties.put("createdate", createDate);
+		
+		ProvenanceGraphActivityNode persistGraphActivityNode = this.provenanceGraphService.createProvenanceGraphActivityNode(activityNodeProvenanceProperties);
+		
+		// Associate Activity with User Agent
+		this.provenanceGraphService.connect(persistGraphActivityNode, userAgentNode, ProvenanceGraphEdgeType.wasAssociatedWith);
+		
+		// Organism
+		Organism org = null;
+		if(organism != null) {
+			if(this.organismService.organismExists(organism)) {
+				org = this.organismService.getOrgansimByOrgCode(organism);
+				this.provenanceGraphService.connect(persistGraphActivityNode, org, ProvenanceGraphEdgeType.used);
+			} else {
+				if(organism.length() != 3) {
+					defaultReturnEntity.setEntityUUID("Organism needs to be threeLetter Organsim Code (i.e. hsa). You priovided: " + organism);
+					returnList.add(defaultReturnEntity);
+					return new ResponseEntity<List<ProvenanceEntity>>(returnList, HttpStatus.BAD_REQUEST);
+				} else {
+					org = this.organismService.createOrganism(organism);
+					this.provenanceGraphService.connect(org, persistGraphActivityNode, ProvenanceGraphEdgeType.wasGeneratedBy);
+				}
+			}
+		} else { // organism is null
+			defaultReturnEntity.setEntityUUID("Need to provide organsim in three Letter Code");
+			returnList.add(defaultReturnEntity);
+			return new ResponseEntity<List<ProvenanceEntity>>(returnList, HttpStatus.BAD_REQUEST);
+			// one could potentially read the organism from the file, but for now lets have the uploader provide it!
+		}
+		// now we can link all created Nodes based on GraphBaseEntity to this organism
+		
+		
+		// MatchingAttributes
+		/**
+		 * So far, I only use one matching attribute.
+		 * I want to have the possibility open to have individual matchingAttributes per Entity in the model
+		 * So far I do not use it, but want to have it covered for future features
+		 */
+		Map<String, String> matchingAttributes = new HashMap<>();
+		if (matchingAttribute != null) {
+			matchingAttributes.put("Default", matchingAttribute);
+		} else {
+			matchingAttributes.put("Default", "N/A");
+		}
+				
+		
+		// KnowledgeGraphNode, not sure if needed actually, the database node could just be the knowledgegraphnode
+		// DatabaseNode has source, version info, connects to KnowledgeGraphNode
+		
+		DatabaseNode database = this.warehouseGraphService.getDatabaseNode(source, sourceVersion, org, matchingAttributes);
+		
+		if(database == null) {
+			// should be only the first time
+			logger.info("Creating DatabaseNode for source: " + source + " with version " + sourceVersion + " and organism " + org.getOrgCode() + " using " + matchingAttributes.get("Default"));
+			database = this.warehouseGraphService.createDatabaseNode(source, sourceVersion, org, matchingAttributes);
+			this.provenanceGraphService.connect(database, persistGraphActivityNode, ProvenanceGraphEdgeType.wasGeneratedBy);
+		} else {
+			this.provenanceGraphService.connect(persistGraphActivityNode, database, ProvenanceGraphEdgeType.used);
+		}
+		
+		
+	
+		
+		FileNode sbmlFileNode = null;
 		// Does node with this filename already exist?
-		if(!sbmlService.sbmlFileNodeExists(file.getOriginalFilename())) {
+		if(!this.warehouseGraphService.fileNodeExists(FileNodeType.SBML, org, file.getOriginalFilename())) {
 			// does not exist -> create
 			try {
-				sbmlFileNode = sbmlService.createSbmlFileNode(file);
+				sbmlFileNode = this.warehouseGraphService.createFileNode(FileNodeType.SBML, org, file.getOriginalFilename(), file.getBytes());
+				this.provenanceGraphService.connect(sbmlFileNode, persistGraphActivityNode, ProvenanceGraphEdgeType.wasGeneratedBy);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				defaultReturnEntity.setEntityUUID("Cannot extract content of file " + file.getOriginalFilename());
 				returnList.add(defaultReturnEntity);
-				return new ResponseEntity<List<GraphBaseEntity>>(returnList, HttpStatus.BAD_REQUEST);
+				return new ResponseEntity<List<ProvenanceEntity>>(returnList, HttpStatus.BAD_REQUEST);
 			}
 			
 		} else {
-			sbmlFileNode = sbmlService.getSbmlFileNode(file.getOriginalFilename());
+			sbmlFileNode = this.warehouseGraphService.getFileNode(FileNodeType.SBML, org, file.getOriginalFilename());
+			this.provenanceGraphService.connect(persistGraphActivityNode, sbmlFileNode, ProvenanceGraphEdgeType.used);
 		}
-		returnList.add(sbmlFileNode);
+		// FileNode needs to connect to DatabaseNode
+		// what if already connected?
+		this.warehouseGraphService.connect(database, sbmlFileNode, WarehouseGraphEdgeType.CONTAINS);
+		
+		
+		List<String> newEntityLabels = new ArrayList<>();
+			
 		Model sbmlModel = null;
 		try {
 			sbmlModel =  sbmlService.extractSBMLModel(file);
@@ -158,22 +262,40 @@ public class LoadDataController {
 			e.printStackTrace();
 			defaultReturnEntity.setEntityUUID("XMLStreamException while extracting SBMLModel from file");
 			returnList.add(defaultReturnEntity);
-			return new ResponseEntity<List<GraphBaseEntity>>(returnList, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<List<ProvenanceEntity>>(returnList, HttpStatus.BAD_REQUEST);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			defaultReturnEntity.setEntityUUID("IOException while extracting SBMLModel from file");
 			returnList.add(defaultReturnEntity);
-			return new ResponseEntity<List<GraphBaseEntity>>(returnList, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<List<ProvenanceEntity>>(returnList, HttpStatus.BAD_REQUEST);
 		}
 		if(sbmlModel == null) {
 			defaultReturnEntity.setEntityUUID("Could not extract an sbmlModel");
 			returnList.add(defaultReturnEntity);
-			return new ResponseEntity<List<GraphBaseEntity>>(returnList, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<List<ProvenanceEntity>>(returnList, HttpStatus.BAD_REQUEST);
 		}
-		List<GraphBaseEntity> resultSet = sbmlService.buildAndPersist(sbmlModel, sbmlFileNode);
-		logger.info("Peristed " + resultSet.size() + " entities for file " + file.getOriginalFilename());
-		return new ResponseEntity<List<GraphBaseEntity>>(resultSet, HttpStatus.OK);
+		List<ProvenanceEntity> resultSet = sbmlService.buildAndPersist(sbmlModel, sbmlFileNode, persistGraphActivityNode);
+		
+		PathwayNode pathwayNode = this.warehouseGraphService.createPathwayNode(sbmlModel.getId(), sbmlModel.getName(), org);
+		
+		for (ProvenanceEntity entity : resultSet) {
+			this.warehouseGraphService.connect(pathwayNode, entity, WarehouseGraphEdgeType.CONTAINS);
+		}
+		
+		logger.info("Persisted " + resultSet.size() + " entities for file " + file.getOriginalFilename());
+		
+		
+		
+		this.provenanceGraphService.connect(pathwayNode, persistGraphActivityNode, ProvenanceGraphEdgeType.wasGeneratedBy);
+		this.provenanceGraphService.connect(pathwayNode, userAgentNode, ProvenanceGraphEdgeType.wasAttributedTo);
+		this.provenanceGraphService.connect(sbmlFileNode, userAgentNode, ProvenanceGraphEdgeType.wasAttributedTo);
+		this.provenanceGraphService.connect(database, userAgentNode, ProvenanceGraphEdgeType.wasAttributedTo);
+		
+		this.warehouseGraphService.connect(sbmlFileNode, pathwayNode, WarehouseGraphEdgeType.CONTAINS);
+		
+		
+		return new ResponseEntity<List<ProvenanceEntity>>(resultSet, HttpStatus.OK);
 	}
 	
 }
