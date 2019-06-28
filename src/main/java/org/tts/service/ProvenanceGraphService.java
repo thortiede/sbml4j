8e960d8bd61ca387c4964712012febfcdd997e7f
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-06-28 16:05:05 +0200

    Service to handle Creation of the ProvenanceGraph

---
+++ src/main/java/org/tts/service/ProvenanceGraphService.java
@@ -0,0 +1,133 @@
+package org.tts.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tts.model.common.GraphEnum.ProvenanceGraphActivityType;
+import org.tts.model.common.GraphEnum.ProvenanceGraphAgentType;
+import org.tts.model.common.GraphEnum.ProvenanceGraphEdgeType;
+import org.tts.model.provenance.ProvenanceEntity;
+import org.tts.model.provenance.ProvenanceGraphActivityNode;
+import org.tts.model.provenance.ProvenanceGraphAgentNode;
+import org.tts.model.provenance.ProvenanceGraphEdge;
+import org.tts.repository.provenance.ProvenanceEntityRepository;
+import org.tts.repository.provenance.ProvenanceGraphActivityNodeRepository;
+import org.tts.repository.provenance.ProvenanceGraphAgentNodeRepository;
+
+@Service
+public class ProvenanceGraphService {
+
+	@Autowired
+	private ProvenanceEntityRepository provenanceEntityRepository;
+	@Autowired
+	private ProvenanceGraphAgentNodeRepository provenanceGraphAgentNodeRepository;
+	@Autowired
+	private SBMLSimpleModelUtilityServiceImpl sbmlSimpleModelUtilityServiceImpl;
+	@Autowired
+	private ProvenanceGraphActivityNodeRepository provenanceGraphActivityNodeRepository;
+	
+	
+	/**
+	 * Creates a ProvenanceGraphAgentNode if it not already exists for that type and the given name
+	 * If it exists, it adds missing Properties from agentNodeProperties to the node
+	 * 
+	 * @param agentNodeProperties
+	 * @return
+	 */
+	public ProvenanceGraphAgentNode createProvenanceGraphAgentNode(
+			Map<String, Object> agentNodeProperties) {
+		
+		switch ((ProvenanceGraphAgentType)agentNodeProperties.get("graphagenttype")) {
+		case User:
+			//String graphagenttype = ((ProvenanceGraphAgentType)agentNodeProperties.get("graphagenttype")).name();
+			ProvenanceGraphAgentNode provenanceGraphAgentNode = 
+			this.provenanceGraphAgentNodeRepository.findByGraphAgentTypeAndGraphAgentName(
+					(ProvenanceGraphAgentType)agentNodeProperties.get("graphagenttype"), 
+					agentNodeProperties.get("graphagentname").toString()
+					);
+
+			if (provenanceGraphAgentNode == null) {
+				provenanceGraphAgentNode = new ProvenanceGraphAgentNode();
+				this.sbmlSimpleModelUtilityServiceImpl.setGraphBaseEntityProperties(provenanceGraphAgentNode);
+				provenanceGraphAgentNode.setGraphAgentType((ProvenanceGraphAgentType) agentNodeProperties.get("graphagenttype"));
+				provenanceGraphAgentNode.setGraphAgentName((String) agentNodeProperties.get("graphagentname"));
+				return this.provenanceGraphAgentNodeRepository.save(provenanceGraphAgentNode);
+			} else {
+				return provenanceGraphAgentNode;
+			}
+			
+			
+		
+			
+		case Organisation:
+			break;
+		case SoftwareAgent:
+			break;
+		default:
+			break;
+		}
+		return null;
+	}
+
+
+	/**
+	 * Creates a ProvenanceGraphActivityNode if not already present for Key: graphactivitytype and 
+	 * @param activityNodeProperties Properties map to be put in the provenance properties filed of the node
+	 * @param activityType the type of the activity from GraphEnum.ProvenanceGraphActivityType
+	 * @return the persisted node
+	 */
+	public ProvenanceGraphActivityNode createProvenanceGraphActivityNode(
+			Map<String, Object> activityNodeProperties) {
+
+		switch ((ProvenanceGraphActivityType) activityNodeProperties.get("graphactivitytype")) {
+		case persistFile:
+			ProvenanceGraphActivityNode provenanceGraphActivityNode = 
+			this.provenanceGraphActivityNodeRepository.findByGraphActivityTypeAndGraphActivityName(
+					(ProvenanceGraphActivityType) activityNodeProperties.get("graphactivitytype"),
+					activityNodeProperties.get("graphactivityname").toString()
+					);
+
+			if(provenanceGraphActivityNode == null) {
+				provenanceGraphActivityNode = new ProvenanceGraphActivityNode();
+				this.sbmlSimpleModelUtilityServiceImpl.setGraphBaseEntityProperties(provenanceGraphActivityNode);
+			}
+			provenanceGraphActivityNode.setGraphActivityType((ProvenanceGraphActivityType) activityNodeProperties.get("graphactivitytype"));
+			provenanceGraphActivityNode.setGraphActivityName((String) activityNodeProperties.get("graphactivityname"));
+			
+			return this.provenanceGraphActivityNodeRepository.save(provenanceGraphActivityNode);
+			
+		case createMapping:
+			break;
+		case mapAnnotations:
+			break;
+		case runAlgorithm:
+			break;
+		default:
+			break;
+		}
+		return null;
+	}
+
+
+	/**
+	 * connect two provenance entities. The entity is not returned. 
+	 * Should that be required in the future, it needs to be changed here
+	 * @param source the startNode of the edge
+	 * @param target the endNode of the edge
+	 * @param edgetype the provenanceGraphEdgeType
+	 */
+	public void connect(ProvenanceEntity source, ProvenanceEntity target,
+			ProvenanceGraphEdgeType edgetype) {
+		// TODO: Make sure Edge does not exist yet...
+		if (!this.provenanceEntityRepository.areProvenanceEntitiesConnectedWithProvenanceEdgeType(source.getEntityUUID(), target.getEntityUUID(), edgetype)) {
+			ProvenanceGraphEdge newEdge = new ProvenanceGraphEdge();
+			this.sbmlSimpleModelUtilityServiceImpl.setGraphBaseEntityProperties(newEdge);
+			newEdge.setProvenanceGraphEdgeType(edgetype);
+			newEdge.setStartNode(source);
+			newEdge.setEndNode(target);
+			
+			this.provenanceEntityRepository.save(newEdge, 0);
+		}
+	}
+}
