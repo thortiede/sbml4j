c988569f934811b682a9c88ef5387023c17c0a6d
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-06-30 23:10:01 +0200

    add findAllPathwaysAttributedToUser method

--- src/main/java/org/tts/repository/warehouse/PathwayNodeRepository.java
+++ src/main/java/org/tts/repository/warehouse/PathwayNodeRepository.java
@@ -1,8 +1,22 @@
 package org.tts.repository.warehouse;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.tts.model.warehouse.PathwayNode;
 
 public interface PathwayNodeRepository extends Neo4jRepository<PathwayNode, Long> {
+
+	
+	/**
+	 * match (p:PathwayNode)-[:PROV {provenanceGraphEdgeType: "wasAttributedTo"}]-(:PROV_AGENT {graphAgentName: "Thor"}) return p
+	 * @param username
+	 * @return
+	 */
+	@Query(value = "MATCH "
+			+ "(p:PathwayNode)"
+			+ "-[:PROV {provenanceGraphEdgeType: \"wasAttributedTo\"}]-"
+			+ "(:PROV_AGENT {graphAgentName: {0}})"
+			+ "RETURN p")
+	Iterable<PathwayNode> findAllPathwaysAttributedToUser(String username);
 
 }
