76aedba7816c71b006e7cd447d4f2b41a5747968
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-06-30 23:12:22 +0200

    Custom Query for getAllFlatTransitionsForPathway

--- src/main/java/org/tts/repository/common/GraphBaseEntityRepository.java
+++ src/main/java/org/tts/repository/common/GraphBaseEntityRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.tts.model.api.Output.NodeNodeEdge;
 import org.tts.model.common.GraphBaseEntity;
+import org.tts.model.common.GraphEnum.ExternalResourceType;
 
 public interface GraphBaseEntityRepository extends Neo4jRepository<GraphBaseEntity, Long> {
 
@@ -89,7 +90,8 @@ public interface GraphBaseEntityRepository extends Neo4jRepository<GraphBaseEnti
 	 */
 	
 	/**
-	 * match (p:PathwayNode {pathwayIdString: "path_hsa05100"})-[:Warehouse {warehouseGraphEdgeType: "CONTAINS"}]-(c:SBMLSimpleTransition) with c match x= (e1:ExternalResourceEntity {type: "kegg.genes"})-[bq1:BQ]-(s1:SBMLSpecies)-[:IS]-(q1:SBMLQualSpecies)-[tr1:IS_INPUT]-(c)-[tr2:IS_OUTPUT ]-(q2:SBMLQualSpecies)-[:IS]-(s2:SBMLSpecies)-[bq2:BQ]-(e2:ExternalResourceEntity {type: "kegg.genes"}) where bq1.qualifier IN ["BQB_HAS_VERSION", "BQB_IS"] and bq2.qualifier IN ["BQB_HAS_VERSION", "BQB_IS"] return e1.name, bq1.qualifier, s1.sBaseName, type(tr1), c.sBaseSboTerm, c.entityUUID, type(tr2), s2.sBaseName, bq2.qualifier, e2.name
+	 * match (p:PathwayNode {pathwayIdString: "path_hsa04210"})-[:Warehouse {warehouseGraphEdgeType: "CONTAINS"}]-(c:SBMLSimpleTransition) with c match x= (e1:ExternalResourceEntity {type: "kegg.genes"})-[bq1:BQ]-(s1:SBMLSpecies)-[:IS]-(q1:SBMLQualSpecies)-[tr1:IS_INPUT]-(c)-[tr2:IS_OUTPUT ]-(q2:SBMLQualSpecies)-[:IS]-(s2:SBMLSpecies)-[bq2:BQ]-(e2:ExternalResourceEntity {type: "kegg.genes"}) where bq1.qualifier IN ["BQB_HAS_VERSION", "BQB_IS", "BQB_IS_ENCODED_BY"] and bq2.qualifier IN ["BQB_HAS_VERSION", "BQB_IS", "BQB_IS_ENCODED_BY"] return e1.name,e1.entityUUID, bq1.qualifier, s1.sBaseName, s1.entityUUID, type(tr1), c.sBaseSboTerm, c.entityUUID, type(tr2), s2.sBaseName, s2.entityUUID, bq2.qualifier, e2.name, e2.entityUUID
+	 * match (p:PathwayNode {pathwayIdString: "path_hsa04210"})-[:Warehouse {warehouseGraphEdgeType: "CONTAINS"}]-(c:SBMLSimpleTransition) with c match x= (e1:ExternalResourceEntity {type: "kegg.genes"})-[bq1:BQ]-(s1:SBMLSpecies)-[:IS]-(q1:SBMLQualSpecies)-[tr1:IS_INPUT]-(c)-[tr2:IS_OUTPUT ]-(q2:SBMLQualSpecies)-[:IS]-(s2:SBMLSpecies)-[bq2:BQ]-(e2:ExternalResourceEntity {type: "kegg.genes"}) where bq1.qualifier IN ["BQB_HAS_VERSION", "BQB_IS", "BQB_IS_ENCODED_BY"] and bq2.qualifier IN ["BQB_HAS_VERSION", "BQB_IS", "BQB_IS_ENCODED_BY"] return e1, s1, q1, c, q2, s2, e2
 	 * 
 	 * 	match 	(p:PathwayNode {pathwayIdString: "path_hsa05100"})
 	 * 				-[:Warehouse {warehouseGraphEdgeType: "CONTAINS"}]-
@@ -110,18 +112,45 @@ public interface GraphBaseEntityRepository extends Neo4jRepository<GraphBaseEnti
 	 *  			-[bq2:BQ]-
 	 *  		(e2:ExternalResourceEntity {type: "kegg.genes"}) 
 	 * 
-	 *  where 	bq1.qualifier IN ["BQB_HAS_VERSION", "BQB_IS"] 
-	 *    and 	bq2.qualifier IN ["BQB_HAS_VERSION", "BQB_IS"] 
-	 * return 	e1.name, 
+	 *  where 	bq1.qualifier IN ["BQB_HAS_VERSION", "BQB_IS", "BQB_IS_ENCODED_BY"] 
+	 *    and 	bq2.qualifier IN ["BQB_HAS_VERSION", "BQB_IS", "BQB_IS_ENCODED_BY"] 
+	 * return 	e1.name,
+	 * 			e1.entityUUID, 
 	 * 			bq1.qualifier, 
 	 * 			s1.sBaseName, 
+	 * 			s1.entityUUID, 
 	 * 			type(tr1), 
 	 * 			c.sBaseSboTerm, 
 	 * 			c.entityUUID, 
 	 * 			type(tr2), 
 	 * 			s2.sBaseName, 
+	 * 			s2.entityUUID, 
 	 * 			bq2.qualifier, 
-	 * 			e2.name
+	 * 			e2.name, 
+	 * 			e2.entityUUID
+	 * 
+	 * 
+	 * alternative return e1, s1, q1, c, q2, s2, e2
 	 */
-	
+	@Query(value="MATCH"
+			+ "(p:PathwayNode {entityUUID: {0}})"
+			+ "-[:Warehouse {warehouseGraphEdgeType: \"CONTAINS\"}]-"
+			+ "(t:SBMLSimpleTransition) "
+			+ "WITH t"
+			+ "MATCH"
+			+ "(e1:ExternalResourceEntity {type: {1}})"
+			+ "-[bq1:BQ]-(s1:SBMLSpecies)-[:IS]-(q1:SBMLQualSpecies)-[tr1:IS_INPUT]-"
+			+ "(t)"
+			+ "-[tr2:IS_OUTPUT ]-(q2:SBMLQualSpecies)-[:IS]-(s2:SBMLSpecies)-[bq2:BQ]-"
+			+ "(e2:ExternalResourceEntity {type: {1}})"
+			+ "WHERE bq1.qualifier IN [\"BQB_HAS_VERSION\", \"BQB_IS\", \"BQB_IS_ENCODED_BY\"]"
+			+ "AND bq2.qualifier IN [\"BQB_HAS_VERSION\", \"BQB_IS\", \"BQB_IS_ENCODED_BY\"]"
+			+ "RETURN"
+			+ "e1.name AS node1"
+			+ "e1.entityUUID as node1UUID"
+			+ "t.ssBaseSboTerm as edge"
+			+ "e2.name as node2"
+			+ "e2.entityUUID as node2UUID"
+			)
+	Iterable<NodeNodeEdge> getAllFlatTransitionsForPathway(String pathwayEntityUUID, ExternalResourceType erType);
 }
