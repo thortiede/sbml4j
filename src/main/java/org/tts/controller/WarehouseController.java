5f58643924c537bd70eabd74a021265e657c069a
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-07-01 12:51:39 +0200

    getPathwayContents endpoint

--- src/main/java/org/tts/controller/WarehouseController.java
+++ src/main/java/org/tts/controller/WarehouseController.java
@@ -3,11 +3,17 @@ package org.tts.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.tts.model.api.Output.PathwayInventoryItem;
+import org.tts.model.common.GraphBaseEntity;
+import org.tts.model.provenance.ProvenanceEntity;
+import org.tts.model.warehouse.PathwayNode;
 import org.tts.service.WarehouseGraphService;
 
 @RestController
@@ -21,6 +27,17 @@ public class WarehouseController {
 	public List<PathwayInventoryItem> listAllPathways(@RequestHeader("user") String username) {
 		
 		return this.warehouseGraphService.getListofPathwayInventory(username);
+		
+	}
+
+	@RequestMapping(value="/pathwayContents", method = RequestMethod.GET)
+	public ResponseEntity<List<ProvenanceEntity>> getPathwayContents(@RequestHeader("user") String username, @RequestParam("uuid") String entityUUID) {
+		PathwayNode pathway = this.warehouseGraphService.getPathwayNode(username, entityUUID);
+		if(pathway == null) {
+			return new ResponseEntity<List<ProvenanceEntity>>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<List<ProvenanceEntity>>(this.warehouseGraphService.getPathwayContents(username, pathway.getEntityUUID()), HttpStatus.OK);
+		}
 		
 	}
 	
