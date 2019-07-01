4919e52de7d70094e7476db593f0b073a94e9ffe
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-07-01 22:45:07 +0200

    derive from base inventory item

--- src/main/java/org/tts/model/api/Output/PathwayInventoryItem.java
+++ src/main/java/org/tts/model/api/Output/PathwayInventoryItem.java
@@ -3,16 +3,12 @@ package org.tts.model.api.Output;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.hateoas.ResourceSupport;
 
-public class PathwayInventoryItem extends ResourceSupport {
 
-	private String entityUUID;
+public class PathwayInventoryItem extends WarehouseInventoryItem {
+
+	
 	private String pathwayId;
-	private String pathwayName;
-	private String pathwayOrganismCode;
-	private String pathwaySource;
-	private String pathwaySourceVersion;
 	private int numberOfNodes = 0;
 	private List<String> nodeTypes = new ArrayList<>();
 	private int numberOfTransitions = 0;
@@ -22,42 +18,14 @@ public class PathwayInventoryItem extends ResourceSupport {
 	private int numberOfCompartments = 0;
 	
 	
-	public String getEntityUUID() {
-		return entityUUID;
-	}
-	public void setEntityUUID(String entityUUID) {
-		this.entityUUID = entityUUID;
-	}
 	public String getPathwayId() {
 		return pathwayId;
 	}
 	public void setPathwayId(String pathwayId) {
 		this.pathwayId = pathwayId;
 	}
-	public String getPathwayName() {
-		return pathwayName;
-	}
-	public void setPathwayName(String pathwayName) {
-		this.pathwayName = pathwayName;
-	}
-	public String getPathwayOrganismCode() {
-		return pathwayOrganismCode;
-	}
-	public void setPathwayOrganismCode(String pathwayOrganismCode) {
-		this.pathwayOrganismCode = pathwayOrganismCode;
-	}
-	public String getPathwaySource() {
-		return pathwaySource;
-	}
-	public void setPathwaySource(String pathwaySource) {
-		this.pathwaySource = pathwaySource;
-	}
-	public String getPathwaySourceVersion() {
-		return pathwaySourceVersion;
-	}
-	public void setPathwaySourceVersion(String pathwaySourceVersion) {
-		this.pathwaySourceVersion = pathwaySourceVersion;
-	}
+	
+	
 	public int getNumberOfNodes() {
 		return numberOfNodes;
 	}
