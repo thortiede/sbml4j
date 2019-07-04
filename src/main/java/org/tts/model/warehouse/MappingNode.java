e95d2e337f17f9d4fd778da671f753737ef81895
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-07-04 22:12:52 +0200

    add info about content to mapping node

--- src/main/java/org/tts/model/warehouse/MappingNode.java
+++ src/main/java/org/tts/model/warehouse/MappingNode.java
@@ -1,5 +1,7 @@
 package org.tts.model.warehouse;
 
+import java.util.Set;
+
 import org.tts.model.common.GraphEnum.NetworkMappingType;
 
 public class MappingNode extends WarehouseGraphNode {
@@ -7,6 +9,35 @@ public class MappingNode extends WarehouseGraphNode {
 	private NetworkMappingType mappingType;
 	
 	private String mappingName;
+	
+	public Set<String> getMappingNodeTypes() {
+		return mappingNodeTypes;
+	}
+
+	public void setMappingNodeTypes(Set<String> mappingNodeTypes) {
+		this.mappingNodeTypes = mappingNodeTypes;
+	}
+
+	public void addMappingNodeType(String type) {
+		this.mappingNodeTypes.add(type);
+	}
+	
+	public Set<String> getMappingRelationTypes() {
+		return mappingRelationTypes;
+	}
+
+	public void setMappingRelationTypes(Set<String> mappingRelationTypes) {
+		this.mappingRelationTypes = mappingRelationTypes;
+	}
+	
+	public void addMappingRelationType(String type) {
+		this.mappingRelationTypes.add(type);
+	}
+
+	private Set<String> mappingNodeTypes;
+	
+	private Set<String> mappingRelationTypes;
+	
 
 	public NetworkMappingType getMappingType() {
 		return mappingType;
