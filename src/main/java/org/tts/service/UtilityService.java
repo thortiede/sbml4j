a24d6f8a9c3f71d6b736009ce42b6329be0d4254
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-07-11 17:29:41 +0200

    new function to translate alias to SBO (but not term)

--- src/main/java/org/tts/service/UtilityService.java
+++ src/main/java/org/tts/service/UtilityService.java
@@ -7,7 +7,13 @@ public class UtilityService {
 
 	
 	public String translateSBOString(String sboString) {
-		return sboString == "" ? "undefined in source" : org.sbml.jsbml.SBO.getTerm(sboString).getName();
+		return (sboString.equals("") || sboString.equals("unknown") || sboString.equals("undefined in source")) ? "undefined in source" : org.sbml.jsbml.SBO.getTerm(sboString).getName();
+	}
+	
+	public String translateToSBOString(String alias) {
+		return org.sbml.jsbml.SBO.intToString(org.sbml.jsbml.SBO.convertAlias2SBO(alias)) != "" ?
+				org.sbml.jsbml.SBO.intToString(org.sbml.jsbml.SBO.convertAlias2SBO(alias)) :
+					"undefined in source"; // error prone TODO not all not known aliases should lead to "undefined in source"
 	}
 	
 }
