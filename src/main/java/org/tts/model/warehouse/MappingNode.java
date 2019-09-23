9edeaa82fef17ea77d50f1f57e567933cd6f5bf7
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-09-23 16:29:55 +0200

    new fields for the context search

--- src/main/java/org/tts/model/warehouse/MappingNode.java
+++ src/main/java/org/tts/model/warehouse/MappingNode.java
@@ -9,6 +9,18 @@ public class MappingNode extends WarehouseGraphNode {
 	private NetworkMappingType mappingType;
 	
 	private String mappingName;
+
+	private Set<String> mappingNodeTypes;
+	
+	private Set<String> mappingRelationTypes;
+	
+	private String baseNetworkEntityUUID;
+	
+	private String geneSymbol;
+	
+	private int minSize;
+	private int maxSize;
+	
 	
 	public Set<String> getMappingNodeTypes() {
 		return mappingNodeTypes;
@@ -34,9 +46,6 @@ public class MappingNode extends WarehouseGraphNode {
 		this.mappingRelationTypes.add(type);
 	}
 
-	private Set<String> mappingNodeTypes;
-	
-	private Set<String> mappingRelationTypes;
 	
 
 	public NetworkMappingType getMappingType() {
@@ -53,6 +62,38 @@ public class MappingNode extends WarehouseGraphNode {
 
 	public void setMappingName(String mappingName) {
 		this.mappingName = mappingName;
+	}
+
+	public String getGeneSymbol() {
+		return geneSymbol;
+	}
+
+	public void setGeneSymbol(String geneSymbol) {
+		this.geneSymbol = geneSymbol;
+	}
+
+	public long getMinSize() {
+		return minSize;
+	}
+
+	public void setMinSize(int minSize) {
+		this.minSize = minSize;
+	}
+
+	public int getMaxSize() {
+		return maxSize;
+	}
+
+	public void setMaxSize(int maxSize) {
+		this.maxSize = maxSize;
+	}
+
+	public String getBaseNetworkEntityUUID() {
+		return baseNetworkEntityUUID;
+	}
+
+	public void setBaseNetworkEntityUUID(String baseNetworkEntityUUID) {
+		this.baseNetworkEntityUUID = baseNetworkEntityUUID;
 	}
 	
 }
