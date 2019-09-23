3daeeb46d4e6b1e9ae7bce9a52c11b93649a98f1
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-09-23 16:30:56 +0200

    extend by minSize and maxSize

--- src/main/java/org/tts/model/api/Input/FilterOptions.java
+++ src/main/java/org/tts/model/api/Input/FilterOptions.java
@@ -16,6 +16,26 @@ public class FilterOptions  extends ResourceSupport {
 	
 	List<String> nodeSymbols;
 
+
+	int minSize;
+	int maxSize;
+	
+	public int getMinSize() {
+		return minSize;
+	}
+
+	public void setMinSize(int minSize) {
+		this.minSize = minSize;
+	}
+
+	public int getMaxSize() {
+		return maxSize;
+	}
+
+	public void setMaxSize(int maxSize) {
+		this.maxSize = maxSize;
+	}
+
 	NetworkMappingType networkType;
 	
 	
