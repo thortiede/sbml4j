230637f62804afe2ab58c4a8be6f94ca9bf81cfd
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-10-07 16:35:09 +0200

    use properties(Node) to get all attributes

--- src/main/java/org/tts/repository/warehouse/MappingNodeRepository.java
+++ src/main/java/org/tts/repository/warehouse/MappingNodeRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.tts.model.api.Output.MappingReturnType;
 import org.tts.model.api.Output.NodeNodeEdge;
 import org.tts.model.flat.FlatSpecies;
 import org.tts.model.warehouse.MappingNode;
@@ -33,10 +34,20 @@ public interface MappingNodeRepository extends Neo4jRepository<MappingNode, Long
 			+ "(m:MappingNode {entityUUID: {0}})"
 			+ "-[:Warehouse {warehouseGraphEdgeType: \"CONTAINS\"}]->"
 			+ "(fs:FlatSpecies)-[r]->(fs2:FlatSpecies) "
+			+ "WHERE type(r) <> \"PROV\" "
 			+ "RETURN fs.symbol as node1, fs.entityUUID as node1UUID, fs.sboTerm as node1Type, "
 			+ "fs2.symbol as node2, fs2.entityUUID as node2UUID, fs2.sboTerm as node2Type, type(r) as edge;")
 	List<NodeNodeEdge> getMappingContentDirected(String mappingNodeEntityUUID);
 
+	@Query(value = "MATCH "
+			+ "(m:MappingNode {entityUUID: {0}})"
+			+ "-[:Warehouse {warehouseGraphEdgeType: \"CONTAINS\"}]->"
+			+ "(fs:FlatSpecies)-[r]->(fs2:FlatSpecies) "
+			+ "WHERE type(r) <> \"PROV\" "
+			+ "RETURN properties(fs) as node1Properties, "
+			+ "properties(fs2) as node2properties, type(r) as edge;")
+	List<MappingReturnType> getMappingContentAsProperties(String mappingNodeEntityUUID);
+	
 	MappingNode getByBaseNetworkEntityUUIDAndGeneSymbolAndMinSizeAndMaxSize(String baseNetworkEntityUUID, String geneSymbol, int minSize, int maxSize);
 	
 	// match (m:MappingNode)-[:PROV]-(p:PROV_AGENT) where p.graphAgentName in ["All", 'Irene'] return m, p;
