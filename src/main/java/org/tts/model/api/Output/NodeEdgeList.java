1c2b4e92b0dbc379e327163aa5e1ea241e1f7b39
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-10-07 16:37:41 +0200

    add annotationType Map

--- src/main/java/org/tts/model/api/Output/NodeEdgeList.java
+++ src/main/java/org/tts/model/api/Output/NodeEdgeList.java
@@ -2,6 +2,7 @@ package org.tts.model.api.Output;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.hateoas.ResourceSupport;
 
@@ -13,6 +14,7 @@ public class NodeEdgeList extends ResourceSupport {
 	
 	private List<NodeNodeEdge> nodeNodeEdgeList;
 	
+	private Map<String, String> annotationType;
 	
 	
 	/**
@@ -53,15 +55,27 @@ public class NodeEdgeList extends ResourceSupport {
 		return nodeNodeEdgeList;
 	}
 
-
-
 	public void setNodeNodeEdgeList(List<NodeNodeEdge> nodeNodeEdgeList) {
 		this.nodeNodeEdgeList = nodeNodeEdgeList;
+	}
+	
+	public Map<String, String> getAnnotationType() {
+		return annotationType;
+	}
+
+	public void setAnnotationType(Map<String, String> annotationType) {
+		this.annotationType = annotationType;
 	}
 	
 	public NodeEdgeList addListEntry(String node1, String node1UUID, String node2, String node2UUID, String edge) {
 		this.getNodeNodeEdgeList().add(new NodeNodeEdge(node1, node1UUID, node2, node2UUID, edge));
 		return this;
 	}
-		
+	
+	public NodeEdgeList addListEntry(String node1, String node1UUID, Map<String, Object> node1Annotation, String node2, String node2UUID, Map<String, Object> node2Annotation, String edge) {
+		this.getNodeNodeEdgeList().add(new NodeNodeEdge(node1, node1UUID, node1Annotation, node2, node2UUID, node2Annotation, edge));
+		return this;
+	}
+	
+	
 }
