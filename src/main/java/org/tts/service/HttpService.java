631d28fc4c9eb463bce067847e312be3c996a777
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-10-07 16:34:35 +0200

    getMyDrugCompounds via Http

--- src/main/java/org/tts/service/HttpService.java
+++ src/main/java/org/tts/service/HttpService.java
@@ -3,10 +3,14 @@ package org.tts.service;
 import java.util.List;
 
 import org.tts.model.common.ExternalResourceEntity;
+import org.tts.model.flat.FlatSpecies;
+import org.tts.model.warehouse.MappingNode;
 
 public interface HttpService {
 
 	public List<String> getGeneNamesFromKeggURL(String resource);
 
-	public ExternalResourceEntity setCompoundAnnotationFromResource(String resource, ExternalResourceEntity entity);	
+	public ExternalResourceEntity setCompoundAnnotationFromResource(String resource, ExternalResourceEntity entity);
+	
+	public List<FlatSpecies> getMyDrugCompoundsForNetwork(String mydrugURL, MappingNode mappingNode);
 }
