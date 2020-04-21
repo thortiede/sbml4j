4277031fa6e3e5b68c722a6a7f26291ac74843c4
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-04-21 20:18:45 +0200

    query for external resources to SBMLSpecies

--- src/main/java/org/tts/repository/common/BiomodelsQualifierRepository.java
+++ src/main/java/org/tts/repository/common/BiomodelsQualifierRepository.java
@@ -3,10 +3,21 @@ package org.tts.repository.common;
 import java.util.List;
 
 import org.sbml.jsbml.CVTerm;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.tts.model.common.BiomodelsQualifier;
 
 public interface BiomodelsQualifierRepository extends Neo4jRepository<BiomodelsQualifier, Long> {
 
 	List<BiomodelsQualifier> findByTypeAndQualifier(CVTerm.Type type, CVTerm.Qualifier qualifier);
+	
+	@Query("MATCH "
+			+ "(s:SBMLSpecies)"
+			+ "-[bq:BQ]-"
+			+ "(e:ExternalResourceEntity) "
+			+ "WHERE "
+			+ "s.entityUUID = $sbmlSpeciesEntityUUID "
+			+ "RETURN bq, e")
+	List<BiomodelsQualifier> findAllForSBMLSpecies(String sbmlSpeciesEntityUUID);
+	
 }
