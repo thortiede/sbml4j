32ee9dce36b4f61fe992993d47d3528f96561ec9
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-07-07 22:32:39 +0200

    general config for vcf processing

--- src/main/java/org/tts/config/VcfConfig.java
+++ src/main/java/org/tts/config/VcfConfig.java
@@ -5,11 +5,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(VcfDefaultProperties.class)
+@EnableConfigurationProperties({VcfDefaultProperties.class, VcfConfigProperties.class})
 public class VcfConfig {
 	@Autowired
 	private VcfDefaultProperties vcfDefaultProperties;
 
+	@Autowired
+	private VcfConfigProperties vcfConfigProperties;
+	
 	public VcfDefaultProperties getVcfDefaultProperties() {
 		return vcfDefaultProperties;
 	}
@@ -17,4 +20,15 @@ public class VcfConfig {
 	public void setVcfDefaultProperties(VcfDefaultProperties vcfDefaultProperties) {
 		this.vcfDefaultProperties = vcfDefaultProperties;
 	}
+
+	public VcfConfigProperties getVcfConfigProperties() {
+		return vcfConfigProperties;
+	}
+
+	public void setVcfConfigProperties(VcfConfigProperties vcfConfigProperties) {
+		this.vcfConfigProperties = vcfConfigProperties;
+	}
+	
+	
+	
 }
