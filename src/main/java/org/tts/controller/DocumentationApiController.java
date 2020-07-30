3be25d87f8871305f212e0fcdc96b58215ea1c5f
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-07-30 17:08:19 +0200

    Add javadoc, license header

--- src/main/java/org/tts/controller/DocumentationApiController.java
+++ src/main/java/org/tts/controller/DocumentationApiController.java
@@ -1,3 +1,24 @@
+/*
+ * ----------------------------------------------------------------------------
+	Copyright 2020 University of Tuebingen 	
+
+	This file is part of SBML4j.
+
+    SBML4j is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SBML4j is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SBML4j.  If not, see <https://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------------- 
+ */
+
 package org.tts.controller;
 
 import java.util.Arrays;
@@ -13,7 +34,12 @@ import org.tts.api.DocumentationApi;
 import org.tts.model.common.GraphBaseEntity;
 import org.tts.service.GraphBaseEntityService;
 
-
+/**
+ * Controller class for handling Requests to the DocumentationApi
+ * @author Thorsten Tiede
+ *
+ * @since 0.1
+ */
 @Controller
 public class DocumentationApiController implements DocumentationApi {
 
@@ -23,21 +49,43 @@ public class DocumentationApiController implements DocumentationApi {
 	@Autowired
 	private GraphBaseEntityService graphBaseEntityService;
 	
-	
+	/**
+	 * get a static representation of the API Description
+	 * 
+	 * @return static HTML page containing api endpoint descriptions
+	 */
 	public ResponseEntity<Object> getBaseDocumentation() {
 		return new ResponseEntity<>("index", HttpStatus.OK);
 	}
 	
-	
+	/**
+	 * get the version of SBML4j
+	 * Needs to be updated manually. For development purposes only.
+	 * 
+	 * @return String containing the current version of SBML4j
+	 */
 	public ResponseEntity<String> getVersion() {
 		return new ResponseEntity<String>("0.0.26-SNAPSHOT", HttpStatus.OK);
 	}
 	
+	/**
+	 * get the list of active profiles
+	 * 
+	 * @return List of profile name strings
+	 */
 	public ResponseEntity<List<String>> getProfile() {
 		List<String> activeProfiles =  Arrays.asList(environment.getActiveProfiles());
 		return new ResponseEntity<>(activeProfiles, HttpStatus.OK);
 	}
 	
+	/**
+	 * Check the status of the connected database.
+	 * 
+	 * This method will attempt to create a simple entity and delete it again.
+	 * If successful, it returns a positive String. Otherwise tries to give a reason for malfunction.
+	 * 
+	 * @return String containing the status of the database
+	 */
 	public ResponseEntity<String> getDbStatus() {
 		try {
 			GraphBaseEntity newEntity = new GraphBaseEntity();
--- src/main/java/org/tts/repository/common/GraphBaseEntityRepository.java
+++ src/main/java/org/tts/repository/common/GraphBaseEntityRepository.java
@@ -1,3 +1,24 @@
+/*
+ * ----------------------------------------------------------------------------
+	Copyright 2020 University of Tuebingen 	
+
+	This file is part of SBML4j.
+
+    SBML4j is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SBML4j is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SBML4j.  If not, see <https://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------------- 
+ */
+
 package org.tts.repository.common;
 
 import java.util.List;
@@ -8,8 +29,21 @@ import org.tts.model.api.Output.MetabolicPathwayReturnType;
 import org.tts.model.api.Output.NonMetabolicPathwayReturnType;
 import org.tts.model.common.GraphBaseEntity;
 
+/**
+ * Repository for handling <a href="#{@link}">{@link GraphBaseEntity}</a>
+ * 
+ * @author Thorsten Tiede
+ *
+ * @since 0.1
+ */
 public interface GraphBaseEntityRepository extends Neo4jRepository<GraphBaseEntity, Long> {
 
+	/**
+	 * Find a <a href="#{@link}">{@link GraphBaseEntity}</a> by its entityUUID attribute
+	 * 
+	 * @param entityUUID the entityUUID to find
+	 * @return <a href="#{@link}">{@link GraphBaseEntity}</a> found for the entityUUID attribute
+	 */
 	GraphBaseEntity findByEntityUUID(String entityUUID);
 	
 	@Query(value="MATCH "
