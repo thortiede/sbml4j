675c59f6072819ab92192d560c36a9f860fb4831
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-07-31 15:58:20 +0200

    cleanup and javadoc

--- src/main/java/org/tts/service/utility/FileCheckService.java
+++ src/main/java/org/tts/service/utility/FileCheckService.java
@@ -1,18 +1,50 @@
-package org.tts.service;
+/*
+ * ----------------------------------------------------------------------------
+	Copyright 2020 University of Tuebingen 	
 
-import java.io.File;
+	This file is part of SBML4j.
+
+    SBML4j is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SBML4j is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SBML4j.  If not, see <https://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------------- 
+ */
+
+package org.tts.service.utility;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * Service to check uploaded files
+ * @author Thorsten Tiede
+ *
+ * @since 0.1
+ */
 @Service
-public class FileCheckServiceImpl implements FileCheckService {
+public class FileCheckService {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@Override
+	/**
+	 * Check if the given <a href="#{@link}">{@link MultipartFile}</a> is readable
+	 * 
+	 * Attemps to read the originalFilename and the ContentType of the <a href="#{@link}">{@link MultipartFile}</a>
+	 * 
+	 * @param file The <a href="#{@link}">{@link MultipartFile}</a> to check
+	 * @return true if originalFilename and ContentType are accessible, false otherwise
+	 */
 	public boolean isFileReadable(MultipartFile file) {
 		try {
 			logger.debug("ContentType of file " + file.getOriginalFilename() + " is " + file.getContentType());
@@ -22,7 +54,12 @@ public class FileCheckServiceImpl implements FileCheckService {
 		return true;
 	}
 
-	@Override
+	/**
+	 * Check whether the ContentType of <a href="#{@link}">{@link MultipartFile}</a> file is xml or sbml
+	 * 
+	 * @param file The <a href="#{@link}">{@link MultipartFile}</a> to check
+	 * @return true if the ContentType is either application/xml, application/sbml+xml or application/sbml, false otherwise
+	 */
 	public boolean isContentXML(MultipartFile file) {
 		if(file.getContentType() != null && (file.getContentType().equals("application/xml")
 												|| file.getContentType().equals("application/sbml+xml")
@@ -32,16 +69,5 @@ public class FileCheckServiceImpl implements FileCheckService {
 			logger.info("file content type is: " + file.getContentType());
 			return false;
 		}
-	
 	}
-
-	@Override
-	public boolean isFileValidSBML(File file) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	
-	
 }
--- src/main/java/org/tts/service/warehouse/OrganismService.java
+++ src/main/java/org/tts/service/warehouse/OrganismService.java
@@ -1,25 +1,53 @@
-package org.tts.service;
+/*
+ * ----------------------------------------------------------------------------
+	Copyright 2020 University of Tuebingen 	
+
+	This file is part of SBML4j.
+
+    SBML4j is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SBML4j is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SBML4j.  If not, see <https://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------------- 
+ */
+
+package org.tts.service.warehouse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tts.model.common.Organism;
 import org.tts.repository.common.OrganismRepository;
+import org.tts.service.GraphBaseEntityService;
 
+/**
+ * Service for handling creation and retrieval of OrgansimNode
+ * @author Thorsten Tiede
+ *
+ * @since 0.1
+ */
 @Service
 public class OrganismService {
 	
-	
+	@Autowired
 	OrganismRepository organismRepository;
-	SBMLSimpleModelUtilityServiceImpl sbmlSimpleModelUtilityService;
 	
 	@Autowired
-	public OrganismService(OrganismRepository organismRepository,
-			SBMLSimpleModelUtilityServiceImpl sbmlSimpleModelUtilityService) {
-		super();
-		this.organismRepository = organismRepository;
-		this.sbmlSimpleModelUtilityService = sbmlSimpleModelUtilityService;
-	}
+	GraphBaseEntityService graphBaseEntityService;
 
+	/**
+	 * check if an Organism with orgCode exists in the database
+	 * 
+	 * @param orgCode The three-letter Organism-code to find
+	 * @return true if Organism with orgCode exists in database, false otherwise
+	 */
 	public boolean organismExists(String orgCode) {
 		if(getOrgansimByOrgCode(orgCode) != null) {
 			return true;
@@ -28,21 +56,32 @@ public class OrganismService {
 		}
 	}
 	
+	/**
+	 * Retrieve an <a href="#{@link}">{@link Organism}</a> from the database
+	 * 
+	 * @param orgCode The three letter organism code of the <a href="#{@link}">{@link Organism}</a> to get
+	 * @return The <a href="#{@link}">{@link Organism}</a> with orgCode
+	 */
 	public Organism getOrgansimByOrgCode(String orgCode) {
 		return organismRepository.findByOrgCode(orgCode);
 	}
 
+	/**
+	 * Create a new <a href="#{@link}">{@link Organism}</a> with three letter code orgCode
+	 * This will return the existing organsim with orgCode if it already exists and create
+	 * a new one otherwise.
+	 * @param orgCode The three letter organism Code
+	 * @return The <a href="#{@link}">{@link Organism}</a> entity with three letter code orgCode
+	 */
 	public Organism createOrganism(String orgCode) {
 		if(organismExists(orgCode)) { // might want to check the orgCode more thoroughly here
 			return getOrgansimByOrgCode(orgCode);
 		} else {
 			Organism newOrganism = new Organism();
-			this.sbmlSimpleModelUtilityService.setGraphBaseEntityProperties(newOrganism);
+			this.graphBaseEntityService.setGraphBaseEntityProperties(newOrganism);
 			newOrganism.setOrgCode(orgCode);
 			// TODO: set more attributes here, maybe query webservice
 			return this.organismRepository.save(newOrganism);
 		}
 	}
-	
-	
 }
