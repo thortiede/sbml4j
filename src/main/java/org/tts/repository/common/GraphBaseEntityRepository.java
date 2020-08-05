ca932c77f303aacfc1af85d3cefdf796386f088c
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-08-05 13:06:51 +0200

    move queries from GraphBase to PathwayNode

--- src/main/java/org/tts/repository/common/GraphBaseEntityRepository.java
+++ src/main/java/org/tts/repository/common/GraphBaseEntityRepository.java
@@ -21,12 +21,7 @@
 
 package org.tts.repository.common;
 
-import java.util.List;
-
-import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.tts.model.api.Output.MetabolicPathwayReturnType;
-import org.tts.model.api.Output.NonMetabolicPathwayReturnType;
 import org.tts.model.common.GraphBaseEntity;
 
 /**
@@ -46,44 +41,5 @@ public interface GraphBaseEntityRepository extends Neo4jRepository<GraphBaseEnti
 	 */
 	GraphBaseEntity findByEntityUUID(String entityUUID);
 	
-	@Query(value="MATCH "
-			+ "(p:PathwayNode)"
-			+ "-[w:Warehouse]->"
-			+ "(t:SBMLSimpleTransition) "
-			+ "WHERE p.entityUUID = $entityUUID "
-			+ "AND w.warehouseGraphEdgeType = \"CONTAINS\" "
-			+ "AND t.sBaseSboTerm IN $transitionSBOTerms "
-			+ "WITH t "
-			+ "MATCH "
-			+ "(s1:SBMLSpecies)-[:IS]-(q1:SBMLQualSpecies)-[tr1:IS_INPUT]-"
-			+ "(t)"
-			+ "-[tr2:IS_OUTPUT ]-(q2:SBMLQualSpecies)-[:IS]-(s2:SBMLSpecies) "
-			+ "WHERE s1.sBaseSboTerm IN $nodeSBOTerms "
-			+ "AND s2.sBaseSboTerm IN $nodeSBOTerms "
-			+ "RETURN "
-			+ "s1 as inputSpecies,"
-			+ "t as transition, "
-			+ "s2 as outputSpecies"
-			)
-	Iterable<NonMetabolicPathwayReturnType> getFlatTransitionsForPathwayUsingSpecies(String entityUUID,
-			List<String> transitionSBOTerms, List<String> nodeSBOTerms);
-		
-	@Query("MATCH "
-			+ " (p:PathwayNode)"
-			+ "-[w:Warehouse]->"
-			+ "(r:SBMLSimpleReaction) "
-			+ "WHERE p.entityUUID = $pathwayEntityUUID "
-			+ "AND w.warehouseGraphEdgeType=\"CONTAINS\" "
-			+ "WITH r "
-			+ "MATCH "
-			+ "(r)"
-			+ "-[rel:IS_PRODUCT|IS_REACTANT|IS_CATALYST]->"
-			+ "(s:SBMLSpecies) "
-			+ "WHERE s.sBaseSboTerm IN $nodeSBOTerms "
-			+ "RETURN s as species, "
-			+ "type(rel) as typeOfRelation, "
-			+ "r as reaction")
-	Iterable<MetabolicPathwayReturnType> getAllMetabolicPathwayReturnTypes(String pathwayEntityUUID,
-			List<String> nodeSBOTerms);
-	
+
 }
--- src/main/java/org/tts/repository/warehouse/PathwayNodeRepository.java
+++ src/main/java/org/tts/repository/warehouse/PathwayNodeRepository.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.tts.model.api.Output.MetabolicPathwayReturnType;
+import org.tts.model.api.Output.NonMetabolicPathwayReturnType;
 import org.tts.model.warehouse.PathwayNode;
 
 public interface PathwayNodeRepository extends Neo4jRepository<PathwayNode, Long> {
@@ -174,5 +176,48 @@ public interface PathwayNodeRepository extends Neo4jRepository<PathwayNode, Long
 			+ "WHERE sb.entityUUID = $sBaseEntityUUID "
 			+ "AND w.warehouseGraphEdgeType = \"CONTAINS\" "
 			+ "RETURN p")
-	List<PathwayNode> getPathwayNodeUUIDsOfSBase(String sBaseEntityUUID);
+	List<PathwayNode> getPathwayNodesOfSBase(String sBaseEntityUUID);
+	
+	
+	@Query(value="MATCH "
+			+ "(p:PathwayNode)"
+			+ "-[w:Warehouse]->"
+			+ "(t:SBMLSimpleTransition) "
+			+ "WHERE p.entityUUID = $pathwayUUID "
+			+ "AND w.warehouseGraphEdgeType = \"CONTAINS\" "
+			+ "AND t.sBaseSboTerm IN $transitionSBOTerms "
+			+ "WITH t "
+			+ "MATCH "
+			+ "(s1:SBMLSpecies)-[:IS]-(q1:SBMLQualSpecies)-[tr1:IS_INPUT]-"
+			+ "(t)"
+			+ "-[tr2:IS_OUTPUT ]-(q2:SBMLQualSpecies)-[:IS]-(s2:SBMLSpecies) "
+			+ "WHERE s1.sBaseSboTerm IN $nodeSBOTerms "
+			+ "AND s2.sBaseSboTerm IN $nodeSBOTerms "
+			+ "RETURN "
+			+ "s1 as inputSpecies,"
+			+ "t as transition, "
+			+ "s2 as outputSpecies"
+			)
+	Iterable<NonMetabolicPathwayReturnType> getFlatTransitionsForPathwayUsingSpecies(String pathwayUUID,
+			List<String> transitionSBOTerms, List<String> nodeSBOTerms);
+		
+	@Query("MATCH "
+			+ " (p:PathwayNode)"
+			+ "-[w:Warehouse]->"
+			+ "(r:SBMLSimpleReaction) "
+			+ "WHERE p.entityUUID = $pathwayUUID "
+			+ "AND w.warehouseGraphEdgeType=\"CONTAINS\" "
+			+ "WITH r "
+			+ "MATCH "
+			+ "(r)"
+			+ "-[rel:IS_PRODUCT|IS_REACTANT|IS_CATALYST]->"
+			+ "(s:SBMLSpecies) "
+			+ "WHERE s.sBaseSboTerm IN $nodeSBOTerms "
+			+ "RETURN s as species, "
+			+ "type(rel) as typeOfRelation, "
+			+ "r as reaction")
+	Iterable<MetabolicPathwayReturnType> getAllMetabolicPathwayReturnTypes(String pathwayUUID,
+			List<String> nodeSBOTerms);
+	
+	
 }
