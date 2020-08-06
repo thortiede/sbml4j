a176fb1fd06bbb43c6a69080c9ae1701219225d4
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-08-06 17:34:10 +0200

    add isMappingNodeAttributedToUser; licence header

--- src/main/java/org/tts/repository/warehouse/MappingNodeRepository.java
+++ src/main/java/org/tts/repository/warehouse/MappingNodeRepository.java
@@ -1,11 +1,30 @@
+/*
+ * ----------------------------------------------------------------------------
+	Copyright 2020 University of Tuebingen 	
+
+	This file is part of SBML4j.
+
+    SBML4j is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SBML4j is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SBML4j.  If not, see <https://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------------- 
+ */
+
 package org.tts.repository.warehouse;
 
 import java.util.List;
 
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.tts.model.api.Output.MappingReturnType;
-import org.tts.model.api.Output.NodeNodeEdge;
 import org.tts.model.flat.FlatSpecies;
 import org.tts.model.warehouse.MappingNode;
 
@@ -21,8 +40,6 @@ public interface MappingNodeRepository extends Neo4jRepository<MappingNode, Long
 			+ "AND w.warehouseGraphEdgeType = \"CONTAINS\" "
 			+ "RETURN fs")
 	List<FlatSpecies> getMappingFlatSpecies(String mappingNodeEntityUUID);
-
-	MappingNode getByBaseNetworkEntityUUIDAndGeneSymbolAndMinSizeAndMaxSize(String baseNetworkEntityUUID, String geneSymbol, int minSize, int maxSize);
 	
 	// match (m:MappingNode)-[:PROV]-(p:PROV_AGENT) where p.graphAgentName in ["All", 'Irene'] return m, p;
 	@Query(value = "MATCH "
@@ -44,5 +61,16 @@ public interface MappingNodeRepository extends Neo4jRepository<MappingNode, Long
 			+ "a.graphAgentName in $users "
 			+ "return m")
 	List<MappingNode> findAllActiveFromUsers(List<String> users);
+	
+	@Query(value = "MATCH "
+			+ "(m:MappingNode)"
+			+ "-[p:PROV]->"
+			+ "(a:PROV_AGENT) "
+			+ "WHERE "
+			+ "m.entityUUID = $entityUUID "
+			+ "AND p.provenanceGraphEdgeType = \"wasAttributedTo\" "
+			+ "AND a.graphAgentName = $user "
+			+ "RETURN count(m) > 0")
+	boolean isMappingNodeAttributedToUser(String entityUUID, String user);
 	
 }
