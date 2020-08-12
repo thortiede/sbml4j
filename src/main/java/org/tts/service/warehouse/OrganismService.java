bfbb683984fe3f9f13b7486d451e4371d22d7c9a
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-08-12 10:42:21 +0200

    add findOrganismForWarhouseGraphNode

--- src/main/java/org/tts/service/warehouse/OrganismService.java
+++ src/main/java/org/tts/service/warehouse/OrganismService.java
@@ -24,6 +24,7 @@ package org.tts.service.warehouse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tts.model.common.Organism;
+import org.tts.model.warehouse.WarehouseGraphNode;
 import org.tts.repository.common.OrganismRepository;
 import org.tts.service.GraphBaseEntityService;
 
@@ -41,31 +42,6 @@ public class OrganismService {
 	
 	@Autowired
 	GraphBaseEntityService graphBaseEntityService;
-
-	/**
-	 * check if an Organism with orgCode exists in the database
-	 * 
-	 * @param orgCode The three-letter Organism-code to find
-	 * @return true if Organism with orgCode exists in database, false otherwise
-	 */
-	public boolean organismExists(String orgCode) {
-		if(getOrgansimByOrgCode(orgCode) != null) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	/**
-	 * Retrieve an <a href="#{@link}">{@link Organism}</a> from the database
-	 * 
-	 * @param orgCode The three letter organism code of the <a href="#{@link}">{@link Organism}</a> to get
-	 * @return The <a href="#{@link}">{@link Organism}</a> with orgCode
-	 */
-	public Organism getOrgansimByOrgCode(String orgCode) {
-		return organismRepository.findByOrgCode(orgCode);
-	}
-
 	/**
 	 * Create a new <a href="#{@link}">{@link Organism}</a> with three letter code orgCode
 	 * This will return the existing organsim with orgCode if it already exists and create
@@ -83,5 +59,39 @@ public class OrganismService {
 			// TODO: set more attributes here, maybe query webservice
 			return this.organismRepository.save(newOrganism);
 		}
+	}
+
+	/**
+	 * Retrieve an <a href="#{@link}">{@link Organism}</a> from the database
+	 * 
+	 * @param orgCode The three letter organism code of the <a href="#{@link}">{@link Organism}</a> to get
+	 * @return The <a href="#{@link}">{@link Organism}</a> with orgCode
+	 */
+	public Organism getOrgansimByOrgCode(String orgCode) {
+		return organismRepository.findByOrgCode(orgCode);
+	}
+	
+	/**
+	 * check if an Organism with orgCode exists in the database
+	 * 
+	 * @param orgCode The three-letter Organism-code to find
+	 * @return true if Organism with orgCode exists in database, false otherwise
+	 */
+	public boolean organismExists(String orgCode) {
+		if(getOrgansimByOrgCode(orgCode) != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Find the <a href="#{@link}">{@link Organism}</a> that is attached to a <a href="#{@link}">{@link WarehouseGraphNode}</a> via the FOR relationship
+	 * 
+	 * @param entityUUID The entityUUID of the <a href="#{@link}">{@link w:WarehouseGraphNode}</a>
+	 * @return The <a href="#{@link}">{@link Organism}</a> found using the query
+	 */
+	public Organism findOrgansimForWarehouseGraphNode(String entityUUID) {
+		return this.organismRepository.findOrganismForWarehouseGraphNode(entityUUID);
 	}
 }
