90dc46a56f94aced21735da466d944bf883392ce
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-08-13 18:59:21 +0200

    add direction parameter to context endpoints

--- src/main/java/org/tts/api/NetworksApi.java
+++ src/main/java/org/tts/api/NetworksApi.java
@@ -263,6 +263,7 @@ public interface NetworksApi {
 ,@ApiParam(value = "The minimum depth of the context search", defaultValue = "1") @Valid @RequestParam(value = "minSize", required = false, defaultValue="1") Integer minSize
 ,@ApiParam(value = "The maximum depth of the context search", defaultValue = "3") @Valid @RequestParam(value = "maxSize", required = false, defaultValue="3") Integer maxSize
 ,@ApiParam(value = "allows to restrict the context search to only show paths that end in a drug node (MyDrug annotations are required for this)", defaultValue = "false") @Valid @RequestParam(value = "terminateAtDrug", required = false, defaultValue="false") Boolean terminateAtDrug
+,@ApiParam(value = "The direction of the context expansion (upstream, downstream, both)", allowableValues = "upstream, downstream, both", defaultValue = "both") @Valid @RequestParam(value = "direction", required = false, defaultValue="both") String direction
 ) {
         if(getObjectMapper().isPresent() && getAcceptHeader().isPresent()) {
             if (getAcceptHeader().get().contains("application/json")) {
@@ -387,6 +388,7 @@ public interface NetworksApi {
 ,@ApiParam(value = "The minimum depth of the context search", defaultValue = "1") @Valid @RequestParam(value = "minSize", required = false, defaultValue="1") Integer minSize
 ,@ApiParam(value = "The maximum depth of the context search", defaultValue = "3") @Valid @RequestParam(value = "maxSize", required = false, defaultValue="3") Integer maxSize
 ,@ApiParam(value = "allows to restrict the context search to only show paths that end in a drug node (MyDrug annotations are required for this)", defaultValue = "false") @Valid @RequestParam(value = "terminateAtDrug", required = false, defaultValue="false") Boolean terminateAtDrug
+,@ApiParam(value = "The direction of the context expansion (upstream, downstream, both)", allowableValues = "upstream, downstream, both", defaultValue = "both") @Valid @RequestParam(value = "direction", required = false, defaultValue="both") String direction
 ) {
         if(getObjectMapper().isPresent() && getAcceptHeader().isPresent()) {
             if (getAcceptHeader().get().contains("application/json")) {
