ad87697803c2f7428267bb1cea30666066c1c74c
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-09-14 17:20:58 +0200

    Swagger generated API Errors and ResponseMessages (unused so far)

---
+++ src/main/java/org/tts/api/ApiException.java
@@ -0,0 +1,10 @@
+package org.tts.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-07-28T10:58:57.976Z[GMT]")
+public class ApiException extends Exception{
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
---
+++ src/main/java/org/tts/api/NotFoundException.java
@@ -0,0 +1,10 @@
+package org.tts.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-07-28T10:58:57.976Z[GMT]")
+public class NotFoundException extends ApiException {
+    private int code;
+    public NotFoundException (int code, String msg) {
+        super(code, msg);
+        this.code = code;
+    }
+}
