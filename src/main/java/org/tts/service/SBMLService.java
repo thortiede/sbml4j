c62e042bdda2d1bfd964fd34108444a5b0c35fdd
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-09-14 18:10:05 +0200

    cleanup unused/old methods

--- src/main/java/org/tts/service/SBMLService.java
+++ src/main/java/org/tts/service/SBMLService.java
@@ -1,16 +1,34 @@
+/*
+ * ----------------------------------------------------------------------------
+	Copyright 2020 University of Tuebingen 	
+
+	This file is part of SBML4j.
+
+    SBML4j is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SBML4j is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SBML4j.  If not, see <https://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------------- 
+ */
+
 package org.tts.service;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import javax.xml.stream.XMLStreamException;
 
 import org.sbml.jsbml.Model;
 import org.springframework.web.multipart.MultipartFile;
-import org.tts.model.common.GraphBaseEntity;
-import org.tts.model.common.SBMLSBaseEntity;
 import org.tts.model.provenance.ProvenanceEntity;
 import org.tts.model.provenance.ProvenanceGraphActivityNode;
 import org.tts.model.warehouse.FileNode;
@@ -22,34 +40,12 @@ public interface SBMLService {
 	public boolean isSBMLVersionCompatible(File file);
 	
 	public Model extractSBMLModel(MultipartFile file) throws XMLStreamException, IOException;
-	
-	public Map<String, Iterable<SBMLSBaseEntity>> extractSBMLEntities(Model model);
-
-	public Map<String, Iterable<Object>> extractAndConnectExternalResources(
-			Map<String, Iterable<SBMLSBaseEntity>> allEntities);
-	
 	/**
-	 * Main entry Method coming from a controller
+	 * Main entry Method coming from a controller to persist a sbmlModel
+	 * 
 	 * @param model The jSBML Model ({@link org.sbml.jsbml.Model}) to persist
 	 * @param sbmlFileNode The name of the file this model originates from
 	 * @return List of GraphBaseEntity that got persisted (or were already in the database) and are part of the model
 	 */
-	public List<ProvenanceEntity> buildAndPersist(Model model, FileNode sbmlFileNode, ProvenanceGraphActivityNode activityNode);
-
-	//public List<GraphBaseEntity> persistFastSimple(Model model);
-	
-	/**
-	 * Remove all Entities from underlying repository
-	 * @return true if database is empty, false otherwise
-	 */
-	public boolean clearDatabase();
-	
-	/**
-	 * Query the underlying repository for all entities of minimal type
-	 * @return List of all entities found in repository which are or are derived from
-	 * {@link org.tts.model.common.GraphBaseEntity}
-	 */
-	public List<GraphBaseEntity> getAllEntities();
-
-	
+	public List<ProvenanceEntity> buildAndPersist(Model model, FileNode sbmlFileNode, ProvenanceGraphActivityNode activityNode);	
 }
