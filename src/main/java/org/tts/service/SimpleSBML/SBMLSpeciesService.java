8589f2169688a90d19a07345cc3885b1e5e1df1d
Thorsten Tiede <thorsten.tiede@uni-tuebingen.de>
2021-01-15 15:33:46 +0100

    method for searching for existing species for new model species

--- src/main/java/org/tts/service/SimpleSBML/SBMLSpeciesService.java
+++ src/main/java/org/tts/service/SimpleSBML/SBMLSpeciesService.java
@@ -13,10 +13,17 @@
  */
 package org.tts.service.SimpleSBML;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.sbml.jsbml.CVTerm;
+import org.sbml.jsbml.CVTerm.Qualifier;
+import org.sbml.jsbml.Species;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tts.config.SBML4jConfig;
@@ -42,6 +49,7 @@ public class SBMLSpeciesService {
 	@Autowired
 	SBMLSpeciesRepository sbmlSpeciesRepository;
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	/**
 	 * Get the <a href="#{@link}">{@link SBMLSpecies}</a> that are part of the group with UUID groupEntityUUID
 	 * @param groupEntityUUID The UUID of the  <a href="#{@link}">{@link SBMLSpeciesGroup}</a>
@@ -123,5 +131,50 @@ public class SBMLSpeciesService {
 			allSpecies.putIfAbsent(species.getEntityUUID(), species);
 		}
 		return allSpecies;
+	}
+
+	public SBMLSpecies findExistingSpeciesToModelSpecies(Species species) {
+		
+		List<String> cvTermUris = new ArrayList<>();
+		// 1. check bqb_is against external resources 
+		for (CVTerm cvTerm : species.getCVTerms()) {
+			//createExternalResources(cvTerm, qualSpecies);
+			if(cvTerm.getBiologicalQualifierType().equals(Qualifier.BQB_IS) || cvTerm.getBiologicalQualifierType().equals(Qualifier.BQB_HAS_VERSION)) {
+				for (String resource : cvTerm.getResources()) {
+					cvTermUris.add(resource);
+				}
+			}
+		}
+		List<SBMLSpecies> foundSBMLSpecies = new ArrayList<>();
+		for (SBMLSpecies existingSpecies :
+			this.sbmlSpeciesRepository.findByBQConnectionWithUriList(cvTermUris)) {
+			// check compartment
+			if(!existingSpecies.getCompartment().getsBaseId().equals(species.getCompartment())) { // this again will not work across different models..
+				continue;
+			}
+			// check sbo term
+			if (!(existingSpecies.getsBaseSboTerm() != null && existingSpecies.getsBaseSboTerm().equals(species.getSBOTermID()))) {
+				continue;
+			}
+			// check more if needed
+			// check sbasename
+			//if (!(existingSpecies.getsBaseId() != null && existingSpecies.getsBaseId().equals(species.getId())))
+			// check sbaseid
+			
+			
+			// if we reach here, we found an existing species that has the same properties in regards to what we check as the new species and we can reuse it.
+			foundSBMLSpecies.add(existingSpecies);
+		}
+		
+		if (foundSBMLSpecies.size() < 1) {
+			logger.debug("Found no matching SBMLSpecies for new entity with sbaseId: " + species.getId());
+			return null;
+		} else if (foundSBMLSpecies.size() > 1) {
+			logger.warn("Found more than one matching species for new entity with sbaseId:" + species.getId());
+			for(SBMLSpecies s : foundSBMLSpecies) {
+				logger.warn("\tSBMLSpecies found with uuid:" + s.getEntityUUID());
+			}
+		}
+		return foundSBMLSpecies.get(0); // this might not be desired in the future. we might want to be able to work with all found matches
 	}
 }
