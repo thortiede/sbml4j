b86fb80d49b4ae41aa3a6700e18bdeb5f9fde17a
Thorsten Tiede <thorsten.tiede@uni-tuebingen.de>
2021-02-19 17:49:25 +0100

    Service for NameNode

---
+++ src/main/java/org/tts/service/NameNodeService.java
@@ -0,0 +1,31 @@
+/**
+ * --------------------------------------------------------------------------
+ *                                 SBML4j
+ * --------------------------------------------------------------------------
+ * University of Tuebingen, 2020.
+ * 
+ * This code is part of the SBML4j software package and subject to the terms
+ * and conditions defined by its license (MIT License). For license details
+ * please refer to the LICENSE file included as part of this source code
+ * package.
+ * 
+ * For a full list of authors, please refer to the file AUTHORS.
+ */
+package org.tts.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tts.model.common.NameNode;
+import org.tts.repository.common.NameNodeRepository;
+
+@Service
+public class NameNodeService {
+
+	@Autowired
+	private NameNodeRepository nameNodeRepository;
+
+	public NameNode findByName(String name) { 
+		return this.nameNodeRepository.findByName(name);
+	}
+
+}
