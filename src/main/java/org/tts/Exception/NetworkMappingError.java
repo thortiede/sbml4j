70459ab6edea45ce2c564b1c335e423666c702e7
Thorsten Tiede <thorsten.tiede@uni-tuebingen.de>
2021-02-23 22:15:02 +0100

    new exception type for erros during network mapping creation

---
+++ src/main/java/org/tts/Exception/NetworkMappingError.java
@@ -0,0 +1,34 @@
+package org.tts.Exception;
+
+public class NetworkMappingError extends Exception {
+
+	/**
+	 * Generated serialVersionUID
+	 */
+	private static final long serialVersionUID = 8375852156926728547L;
+
+	public NetworkMappingError() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NetworkMappingError(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NetworkMappingError(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NetworkMappingError(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NetworkMappingError(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
