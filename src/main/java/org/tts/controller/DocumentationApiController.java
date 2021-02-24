5964bbc1318edfa00da4bdc6b9ea29e8972d3470
Thorsten Tiede <thorsten.tiede@uni-tuebingen.de>
2021-02-24 21:49:57 +0100

    version to 0.2.1

--- src/main/java/org/tts/controller/DocumentationApiController.java
+++ src/main/java/org/tts/controller/DocumentationApiController.java
@@ -57,7 +57,7 @@ public class DocumentationApiController implements DocumentationApi {
 	 * @return String containing the current version of SBML4j
 	 */
 	public ResponseEntity<String> getVersion() {
-		return new ResponseEntity<String>("0.2", HttpStatus.OK);
+		return new ResponseEntity<String>("0.2.1", HttpStatus.OK);
 	}
 	
 	/**
