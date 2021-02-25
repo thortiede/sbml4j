8865fa53d7f4f68339bc7745649e160f16aa7db5
Thorsten Tiede <thorsten.tiede@uni-tuebingen.de>
2021-02-25 23:18:24 +0100

    change in compound; addition for drugInformation fetching

--- src/main/java/org/tts/service/KEGGHttpService.java
+++ src/main/java/org/tts/service/KEGGHttpService.java
@@ -102,7 +102,7 @@ public class KEGGHttpService {
 		}
 	}
 
-	public ExternalResourceEntity setCompoundAnnotationFromResource(String resource, ExternalResourceEntity entity) {
+	public List<String> setCompoundAnnotationFromResource(String resource, ExternalResourceEntity entity) {
 		String[] resourceParts =  resource.split("/");
 		String identifier = resourceParts[resourceParts.length - 1];
 		try {
@@ -115,7 +115,125 @@ public class KEGGHttpService {
 
 			if (status > 299) {
 				logger.error("Cannot get kegg.compound information for " + identifier + ": " + con.getResponseMessage());
-				return entity;
+				return null;
+			} else {
+				streamReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			}
+
+			String inputLine;
+			String[] splitted;
+			String secNameLine;
+			String lastLine = "";
+			List<String> names = new ArrayList<>();
+			String formula;
+			String exactMass;
+			String molWeight;
+			while ((inputLine = streamReader.readLine()) != null) {
+				//logger.debug(inputLine);
+		
+				if(inputLine.startsWith("NAME")) {
+					lastLine = "NAME";
+					// we now assume that every compound entry looks similar to :
+					/* Name	   	Acetoacetate;
+								3-Oxobutanoic acid;
+								beta-Ketobutyric acid;
+								Acetoacetic acid
+					*/
+					
+					inputLine = inputLine.substring(4).strip();
+					if (inputLine.endsWith(",") || inputLine.endsWith(";")) {
+						names.add(inputLine.substring(0, inputLine.length() - 1));
+					} else {
+						names.add(inputLine);
+					}
+				} else if(lastLine.equals("NAME") && inputLine.startsWith(" ")) {
+					//splitted = inputLine.split("\\s+");
+					secNameLine = inputLine.strip();
+					if (secNameLine.endsWith(",") || secNameLine.endsWith(";")) {
+						names.add(secNameLine.substring(0, secNameLine.length() - 1));
+					} else {
+						names.add(secNameLine);
+					}
+				} else if(inputLine.startsWith("FORMULA")) {
+					lastLine = "FORMULA";
+					splitted = inputLine.split("\\s+");
+					if(splitted.length == 2) {
+						if (splitted[1].endsWith(",") || splitted[1].endsWith(";")) {
+							formula = splitted[1].substring(0, splitted[1].length() - 1);
+						} else {
+							formula = splitted[1];
+						}
+						entity.addAnnotation(lastLine, formula);
+						entity.addAnnotationType(lastLine, "String");
+					} else {
+						// Formula should have exactly two elements
+					}
+				} else if(inputLine.startsWith("EXACT_MASS")) {
+					lastLine = "EXACT_MASS";
+					splitted = inputLine.split("\\s+");
+					if(splitted.length == 2) {
+						if (splitted[1].endsWith(",") || splitted[1].endsWith(";")) {
+							exactMass = splitted[1].substring(0, splitted[1].length() - 1);
+						} else {
+							exactMass = splitted[1];
+						}
+						entity.addAnnotation(lastLine, exactMass);
+						entity.addAnnotationType(lastLine, "String");
+					} else {
+						// exactMass should have exactly two elements
+					}
+				} else if(inputLine.startsWith("MOL_WEIGHT")) {
+					lastLine = "MOL_WEIGHT";
+					splitted = inputLine.split("\\s+");
+					if(splitted.length == 2) {
+						if (splitted[1].endsWith(",") || splitted[1].endsWith(";")) {
+							molWeight = splitted[1].substring(0, splitted[1].length() - 1);
+						} else {
+							molWeight = splitted[1];
+						}
+						entity.addAnnotation(lastLine, molWeight);
+						entity.addAnnotationType(lastLine, "String");
+					} else {
+						// molWeight should have exactly two elements
+					}
+				} else {
+					lastLine = "";
+				}
+			}
+			entity.setPrimaryName(names.remove(0));
+			
+			if (names.size() > 0) {
+				return names;
+			} else {
+				return new ArrayList<>();
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+
+	public List<String> getDrugInformationFromKEGGDrugURL(String resource, ExternalResourceEntity entity) {
+		String[] resourceParts =  resource.split("/");
+		String identifier = resourceParts[resourceParts.length - 1];
+		HttpURLConnection con;
+		try {
+			URL url = new URL("http://rest.kegg.jp/get/" + identifier);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			int status = con.getResponseCode();
+
+			BufferedReader streamReader = null;
+
+			if (status > 299) {
+				logger.error("Cannot get kegg.compound information for " + identifier + ": " + con.getResponseMessage());
+				return null;
 			} else {
 				streamReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			}
@@ -200,11 +318,9 @@ public class KEGGHttpService {
 			entity.setPrimaryName(names.remove(0));
 			
 			if (names.size() > 0) {
-				String[] secNames = new String[names.size()];
-				for (int i = 0; i != names.size(); i++) {
-					secNames[i] = names.get(i);
-				}
-				entity.setSecondaryNames(secNames);
+				return names;
+			} else {
+				return new ArrayList<>();
 			}
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
@@ -215,6 +331,5 @@ public class KEGGHttpService {
 			e.printStackTrace();
 			return null;
 		}
-		return entity;
 	}	
 }
