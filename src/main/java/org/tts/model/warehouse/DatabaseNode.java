64ab7063b1d5052e1ab48b21446170c9361cf866
Thorsten Tiede <thorsten.tiede@uni-tuebingen.de>
2021-04-16 17:09:24 +0200

    remove unused properties; add hashCode and equals methods

--- src/main/java/org/tts/model/warehouse/DatabaseNode.java
+++ src/main/java/org/tts/model/warehouse/DatabaseNode.java
@@ -13,13 +13,10 @@
  */
 package org.tts.model.warehouse;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.neo4j.ogm.annotation.Relationship;
-import org.tts.model.common.GraphBaseEntity;
 import org.tts.model.provenance.ProvenanceGraphEntityNode;
 
 public class DatabaseNode extends WarehouseGraphNode {
@@ -27,10 +24,7 @@ public class DatabaseNode extends WarehouseGraphNode {
 	private String source;
 	private String sourceVersion;
 	private Map<String, String> matchingAttributes;
-		
-	private Map<String, String> entityKeyMap;
-	
-	private Map<String, GraphBaseEntity> nameEntityMap;
+
 	
 	@Relationship(type="HAS_ENTITY")
 	private List<ProvenanceGraphEntityNode> entities;
@@ -55,47 +49,6 @@ public class DatabaseNode extends WarehouseGraphNode {
 	}
 
 
-	public Map<String, String> getEntityKeyMap() {
-		return entityKeyMap;
-	}
-
-
-	public void setEntityKeyMap(Map<String, String> entityKeyMap) {
-		this.entityKeyMap = entityKeyMap;
-	}
-
-
-	public boolean contains(String entityName) {
-		if(nameEntityMap != null) {
-			if(nameEntityMap.containsKey(entityName)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-
-	public Map<String, GraphBaseEntity> getNameEntityMap() {
-		if (nameEntityMap != null ) {
-			return nameEntityMap;
-		} else {
-			return new HashMap<>();
-		}
-	}
-
-
-	public void add(String name, GraphBaseEntity entity) {
-		if (entities == null) {
-			entities = new ArrayList<>();
-		}
-		if(!entities.contains(entity)) {
-			//entities.add(entity);
-			entityKeyMap.put(name, entity.getEntityUUID());
-			nameEntityMap.put(name, entity);
-		}
-		
-	}
-
 
 	public Map<String, String> getMatchingAttributes() {
 		return matchingAttributes;
@@ -105,4 +58,46 @@ public class DatabaseNode extends WarehouseGraphNode {
 	public void setMatchingAttributes(Map<String, String> matchingAttributes) {
 		this.matchingAttributes = matchingAttributes;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((matchingAttributes == null) ? 0 : matchingAttributes.hashCode());
+		result = prime * result + ((source == null) ? 0 : source.hashCode());
+		result = prime * result + ((sourceVersion == null) ? 0 : sourceVersion.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DatabaseNode other = (DatabaseNode) obj;
+		if (matchingAttributes == null) {
+			if (other.matchingAttributes != null)
+				return false;
+		} else if (!matchingAttributes.equals(other.matchingAttributes))
+			return false;
+		if (source == null) {
+			if (other.source != null)
+				return false;
+		} else if (!source.equals(other.source))
+			return false;
+		if (sourceVersion == null) {
+			if (other.sourceVersion != null)
+				return false;
+		} else if (!sourceVersion.equals(other.sourceVersion))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
