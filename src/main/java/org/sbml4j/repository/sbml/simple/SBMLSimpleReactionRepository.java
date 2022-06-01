aaaba485aba7c31bd3d6f9ae5b8e53bb2d47ff16
Thorsten Tiede <thorsten.tiede@uni-tuebingen.de>
2022-06-01 17:07:05 +0200

    new query for reactions of pathway

--- src/main/java/org/sbml4j/repository/sbml/simple/SBMLSimpleReactionRepository.java
+++ src/main/java/org/sbml4j/repository/sbml/simple/SBMLSimpleReactionRepository.java
@@ -13,6 +13,8 @@
  */
 package org.sbml4j.repository.sbml.simple;
 
+import java.util.List;
+
 import org.sbml4j.model.queryResult.MetabolicPathwayReturnType;
 import org.sbml4j.model.sbml.simple.SBMLSimpleReaction;
 import org.springframework.data.neo4j.annotation.Query;
@@ -43,4 +45,31 @@ public interface SBMLSimpleReactionRepository extends Neo4jRepository<SBMLSimple
 			+ "type(rel) as typeOfRelation, "
 			+ "r as reaction")
 	Iterable<MetabolicPathwayReturnType> findAllSpeciesForReaction(String reactionEntityUUID);
+
+	@Query("MATCH "
+			+ " (p:PathwayNode)"
+			+ "-[w:Warehouse]->"
+			+ "(r:SBMLSimpleReaction) "
+			+ "WHERE p.entityUUID = $pathwayUUID "
+			+ "AND w.warehouseGraphEdgeType=\"CONTAINS\" "
+			+ "WITH r "
+			+ "MATCH "
+			+ "(r)"
+			+ "-[rel:IS_PRODUCT|IS_REACTANT|IS_CATALYST]->"
+			+ "(s:SBMLSpecies) "
+			+ "WHERE s.sBaseSboTerm IN $partnerSBOTerms "
+			+ "WITH r, rel, s "
+			+ "OPTIONAL MATCH "
+			+ "(s)"
+			+ "-[b:BQ]->"
+			+ "(e:ExternalResourceEntity) "
+			+ "WHERE b.type = \"BIOLOGICAL_QUALIFIER\" "
+			+ "AND b.qualifier IN [\"BQB_HAS_VERSION\", \"BQB_IS\", \"BQB_IS_ENCODED_BY\"] "
+			+ "WITH r, rel, s, b, e "
+			+ "OPTIONAL MATCH "
+			+ "(e)"
+			+ "-[k:KNOWNAS]->"
+			+ "(n:NameNode) "
+			+ "RETURN r, rel, s, b, e, k, n ")
+	Iterable<SBMLSimpleReaction> findAllInPathwayWithPartnerSBOTerm(String pathwayUUID, List<String> partnerSBOTerms);
 }
