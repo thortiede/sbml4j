b21e4b9b386c9a01ad9ac21cdbb1a00dfd29246d
Thorsten Tiede <thorsten.tiede@uni-tuebingen.de>
2022-10-13 12:14:29 +0200

    new Warehouse method for retrieving the filenode to a warehouse node

--- src/main/java/org/sbml4j/controller/WarehouseApiController.java
+++ src/main/java/org/sbml4j/controller/WarehouseApiController.java
@@ -13,11 +13,19 @@
  */
 package org.sbml4j.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.sbml4j.api.WarehouseApi;
+import org.sbml4j.model.base.GraphEnum.ProvenanceGraphEdgeType;
+import org.sbml4j.model.provenance.ProvenanceEntity;
 import org.sbml4j.model.warehouse.DatabaseNode;
+import org.sbml4j.model.warehouse.FileNode;
+import org.sbml4j.service.ProvenanceGraphService;
 import org.sbml4j.service.warehouse.DatabaseNodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -37,6 +45,8 @@ public class WarehouseApiController implements WarehouseApi {
 	@Autowired
 	DatabaseNodeService databaseNodeService;
 	
+	@Autowired
+	ProvenanceGraphService provenanceGraphService;
 	/**
 	 * Get the UUID of the <a href="#{@link}">{@link DatabaseNode}</a> for the given parameters
 	 * 
@@ -46,14 +56,38 @@ public class WarehouseApiController implements WarehouseApi {
 	 * @return The UUID of the <a href="#{@link}">{@link DatabaseNode}</a>
 	 */
 	@Override
-	public ResponseEntity<String> getDatabaseUUID(@NotNull @Valid String organism, @NotNull @Valid String source,
+	public ResponseEntity<UUID> getDatabaseUUID(@NotNull @Valid String organism, @NotNull @Valid String source,
 		@NotNull @Valid String version) {
 
 		DatabaseNode database = this.databaseNodeService.getDatabaseNode(source, version, organism);
 		if (database != null) {
-			return new ResponseEntity<String>(database.getEntityUUID(), HttpStatus.OK);
+			return new ResponseEntity<UUID>(java.util.UUID.fromString(database.getEntityUUID()), HttpStatus.OK);
 		} else {
-			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+			return new ResponseEntity<UUID>(HttpStatus.NOT_FOUND);
 		}
+	}
+
+	@Override
+	public ResponseEntity<List<UUID>> getFileorigin(UUID UUID, String user) {
+		
+		// ignoring the user for now.
+		String uuid = UUID.toString();
+		Iterable<ProvenanceEntity> deriver = this.provenanceGraphService.findAllByProvenanceGraphEdgeTypeAndStartNode(ProvenanceGraphEdgeType.wasDerivedFrom, uuid);
+		if (deriver == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		List<UUID> fileNodesFound = new ArrayList<>();
+		for (ProvenanceEntity possibleFileNode : deriver) {
+			if (possibleFileNode instanceof FileNode) {
+				fileNodesFound.add(java.util.UUID.fromString(possibleFileNode.getEntityUUID()));
+			}
+		}
+		if (fileNodesFound.size() < 1) {
+			return ResponseEntity.notFound().header("reason", "No filenode as origin of node with provided uuid found").build();
+		}
+		
+		return ResponseEntity.ok(fileNodesFound);
+		
 	}
 }
